18439608b757098a26fea8bb9935797f83dd1d9d
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2017-08-30 22:17:29 -0300

    Ported StencilTest sample

--- Samples/2.0/ApiUsage/StencilTest/StencilTestGameState.cpp
+++ Samples/2.0/ApiUsage/StencilTest/StencilTestGameState.cpp
@@ -25,10 +25,6 @@ namespace Demo
     //-----------------------------------------------------------------------------------
     void StencilTestGameState::createScene01(void)
     {
-        //We MUST do this so the renderWindowDepthBuffer & rt_renderwindow can share
-        //the same depth buffer.
-        mGraphicsSystem->getRenderWindow()->setPreferDepthTexture( true );
-
         Ogre::SceneManager *sceneManager = mGraphicsSystem->getSceneManager();
 
         //Make sure both RQs 0 & 1 are able to hold Items.
