79f5330ecf9dccc5404553a65ac8991d378f51ce
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2018-05-13 14:40:07 -0300

    Added double sided support for area lights

--- Components/Hlms/Pbs/src/OgreHlmsPbs.cpp
+++ Components/Hlms/Pbs/src/OgreHlmsPbs.cpp
@@ -1139,14 +1139,15 @@ namespace Ogre
             if( numShadowMapLights > 0 )
             {
                 //Six variables * 4 (padded vec3) * 4 (bytes) * numLights
-                mapSize += ( 6 * 4 * 4 ) * (numLights + numAreaApproxLights);
+                mapSize += ( 6 * 4 * 4 ) * numLights;
             }
             else
             {
                 //Three variables * 4 (padded vec3) * 4 (bytes) * numDirectionalLights
                 mapSize += ( 3 * 4 * 4 ) * numDirectionalLights;
-                mapSize += ( 6 * 4 * 4 ) * numAreaApproxLights;
             }
+
+            mapSize += ( 7 * 4 * 4 ) * numAreaApproxLights;
         }
         else
         {
@@ -1629,6 +1630,12 @@ namespace Ogre
                 *passBufferPtr++ = xAxis.y;
                 *passBufferPtr++ = xAxis.z;
                 *passBufferPtr++ = 1.0f / rectHalfSize.y;
+
+                //vec4 doubleSided;
+                *passBufferPtr++ = light->mDoubleSided ? 1.0f : 0.0f;
+                *passBufferPtr++ = 0.0f;
+                *passBufferPtr++ = 0.0f;
+                *passBufferPtr++ = 0.0f;
             }
 
             for( int32 i=realNumAreaLights; i<numAreaApproxLights; ++i )
@@ -1668,6 +1675,12 @@ namespace Ogre
                 *passBufferPtr++ = 0;
                 *passBufferPtr++ = 0;
                 *passBufferPtr++ = 1.0f;
+
+                //vec4 doubleSided;
+                *passBufferPtr++ = 0.0f;
+                *passBufferPtr++ = 0.0f;
+                *passBufferPtr++ = 0.0f;
+                *passBufferPtr++ = 0.0f;
             }
 
             if( shadowNode )
