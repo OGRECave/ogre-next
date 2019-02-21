7c30d8834ffcad8d1b6e94f95e162bc771f743b1
Eugene Golushkov <e.golushkov@nospam_gmail.com>
2019-02-21 13:40:59 +0200

    [D3D11] Restored the ability to differentiate between identically named adapters

--- RenderSystems/Direct3D11/src/OgreD3D11Driver.cpp
+++ RenderSystems/Direct3D11/src/OgreD3D11Driver.cpp
@@ -30,28 +30,26 @@ THE SOFTWARE.
 #include "OgreD3D11VideoMode.h"
 #include "OgreD3D11Device.h"
 #include "OgreString.h"
+#include "OgreStringConverter.h"
+
+#ifndef DXGI_ADAPTER_FLAG_SOFTWARE
+#define DXGI_ADAPTER_FLAG_SOFTWARE 2 // unavailable in June 2010 SDK
+#endif
+
 namespace Ogre
 {
     //---------------------------------------------------------------------
     D3D11Driver::D3D11Driver() 
     {
         ZeroMemory( &mAdapterIdentifier, sizeof(mAdapterIdentifier) );
+        mSameNameAdapterIndex = 0;
     }
     //---------------------------------------------------------------------
-    D3D11Driver::D3D11Driver( const D3D11Driver &ob ) 
+    D3D11Driver::D3D11Driver(IDXGIAdapterN* pDXGIAdapter, const DXGI_ADAPTER_DESC1& desc, unsigned sameNameIndex)
     {
-        mDXGIAdapter = ob.mDXGIAdapter;
-        mAdapterIdentifier = ob.mAdapterIdentifier;
-        mVideoModeList = ob.mVideoModeList;
-    }
-    //---------------------------------------------------------------------
-    D3D11Driver::D3D11Driver(IDXGIAdapterN* pDXGIAdapter)
-    {
-        ZeroMemory(&mAdapterIdentifier, sizeof(mAdapterIdentifier));
-
         mDXGIAdapter = pDXGIAdapter;
-        if(mDXGIAdapter)
-            mDXGIAdapter->GetDesc1(&mAdapterIdentifier);
+        mAdapterIdentifier = desc;
+        mSameNameAdapterIndex = sameNameIndex;
     }
     //---------------------------------------------------------------------
     D3D11Driver::~D3D11Driver()
@@ -65,16 +63,17 @@ namespace Ogre
         str[sizeof(str) - 1] = '\0';
         String driverDescription=str;
         StringUtil::trim(driverDescription);
-#if OGRE_COMP_VER > 1500
-        if(mAdapterIdentifier.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
-            driverDescription += " (software)";
-#endif
+        if(mAdapterIdentifier.VendorId == 0x1414 && mAdapterIdentifier.DeviceId == 0x8c && (mAdapterIdentifier.Flags & DXGI_ADAPTER_FLAG_SOFTWARE))
+            driverDescription += " (software)"; // It`s software only variation of "Microsoft Basic Render Driver", always present since Win8
+        else if(mSameNameAdapterIndex != 0)
+            driverDescription += " (" + Ogre::StringConverter::toString(mSameNameAdapterIndex + 1) + ")";
+
         return driverDescription;
     }
     //---------------------------------------------------------------------
     D3D11VideoModeList* D3D11Driver::getVideoModeList()
     {
-        if(mVideoModeList.isNull())
+        if(!mVideoModeList)
             mVideoModeList = SharedPtr<D3D11VideoModeList>(OGRE_NEW_T(D3D11VideoModeList, MEMCATEGORY_GENERAL)(getDeviceAdapter()), SPFM_DELETE_T);
 
         return mVideoModeList.get();
