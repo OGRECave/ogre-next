b44b4dda7d5646fcde2d4f0af453412c70a0dfe8
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2019-08-03 19:08:07 -0300

    Added build scripts to automate download & building process

---
+++ Scripts/BuildScripts/output/build_ogre_Visual_Studio_9_2008_x64.bat
@@ -0,0 +1,68 @@
+
+@echo off
+SETLOCAL
+
+set OGRE_BRANCH_NAME=v2-2
+set GENERATOR="Visual Studio 9 2008"
+set PLATFORM=x64
+
+set CMAKE_BIN_x86="C:\Program Files (x86)\CMake\bin\cmake.exe"
+set CMAKE_BIN_x64="C:\Program Files\CMake\bin\cmake.exe"
+IF EXIST %CMAKE_BIN_x64% (
+	echo CMake 64-bit detected
+	set CMAKE_BIN=%CMAKE_BIN_x64%
+) ELSE (
+	IF EXIST %CMAKE_BIN_x86% (
+		echo CMake 32-bit detected
+		set CMAKE_BIN=%CMAKE_BIN_x86%
+	) ELSE (
+		echo Cannot detect either %CMAKE_BIN_x86% or
+		echo %CMAKE_BIN_x64% make sure CMake is installed
+		EXIT /B 1
+	)
+)
+echo Using CMake at %CMAKE_BIN%
+
+mkdir Ogre
+cd Ogre
+IF NOT EXIST ogredeps (
+	mkdir ogredeps
+	echo --- Cloning Ogredeps ---
+	hg clone https://bitbucket.org/cabalistic/ogredeps ogredeps
+) ELSE (
+	echo --- Ogredeps repo detected. Cloning skipped ---
+)
+cd ogredeps
+mkdir build
+cd build
+echo --- Building Ogredeps ---
+%CMAKE_BIN% -G %GENERATOR% -A %PLATFORM% ..
+%CMAKE_BIN% --build . --config Debug
+%CMAKE_BIN% --build . --target install --config Debug
+%CMAKE_BIN% --build . --config Release
+%CMAKE_BIN% --build . --target install --config Release
+
+cd ../../
+IF NOT EXIST ogre (
+	mkdir ogre
+	echo --- Cloning Ogre v2-2 ---
+	hg clone https://bitbucket.org/sinbad/ogre -r %OGRE_BRANCH_NAME% ogre
+)
+cd ogre
+IF NOT EXIST Dependencies (
+	mklink /D Dependencies ..\ogredeps\build\ogredeps
+)
+mkdir build
+cd build
+echo --- Running CMake configure ---
+%CMAKE_BIN% -D OGRE_USE_BOOST=0 -D OGRE_CONFIG_THREAD_PROVIDER=0 -D OGRE_CONFIG_THREADS=0 -D OGRE_BUILD_COMPONENT_SCENE_FORMAT=1 -D OGRE_BUILD_SAMPLES2=1 -D OGRE_BUILD_TESTS=1 -D OGRE_DEPENDENCIES_DIR=..\ogredeps\build\ogredeps -G %GENERATOR% -A %PLATFORM% ..
+echo --- Building Ogre (Debug) ---
+%CMAKE_BIN% --build . --config Debug
+%CMAKE_BIN% --build . --target install --config Debug
+echo --- Building Ogre (Release) ---
+%CMAKE_BIN% --build . --config Release
+%CMAKE_BIN% --build . --target install --config Release
+
+echo Done!
+
+ENDLOCAL
---
+++ Scripts/BuildScripts/output/build_ogre_linux_c++98.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+OGRE_BRANCH_NAME="v2-2"
+
+mkdir Ogre
+cd Ogre
+if test ! -f ogredeps; then
+	mkdir ogredeps
+	echo "--- Cloning Ogredeps ---"
+	hg clone https://bitbucket.org/cabalistic/ogredeps ogredeps
+else
+	echo "--- Ogredeps repo detected. Cloning skipped ---"
+fi
+cd ogredeps
+mkdir build
+cd build
+echo "--- Building Ogredeps ---"
+cmake -D CMAKE_CXX_STANDARD=98 -G Ninja ..
+ninja
+ninja install
+
+cd ../../
+if test ! -f ogre; then
+	mkdir ogre
+	echo "--- Cloning Ogre v2-2 ---"
+	hg clone https://bitbucket.org/sinbad/ogre -r ${OGRE_BRANCH_NAME} ogre
+fi
+cd ogre
+if test ! -f Dependencies; then
+	ln -s ../ogredeps/build/ogredeps Dependencies
+fi
+mkdir -p build/Debug
+mkdir -p build/Release
+cd build/Debug
+echo "--- Building Ogre (Debug) ---"
+cmake -D OGRE_USE_BOOST=0 -D OGRE_CONFIG_THREAD_PROVIDER=0 -D OGRE_CONFIG_THREADS=0 -D OGRE_BUILD_COMPONENT_SCENE_FORMAT=1 -D OGRE_BUILD_SAMPLES2=1 -D OGRE_BUILD_TESTS=1 -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_CXX_STANDARD=98 -G Ninja ../..
+ninja
+cd ../Release
+echo "--- Building Ogre (Release) ---"
+cmake -D OGRE_USE_BOOST=0 -D OGRE_CONFIG_THREAD_PROVIDER=0 -D OGRE_CONFIG_THREADS=0 -D OGRE_BUILD_COMPONENT_SCENE_FORMAT=1 -D OGRE_BUILD_SAMPLES2=1 -D OGRE_BUILD_TESTS=1 -D CMAKE_BUILD_TYPE="Release" -D CMAKE_CXX_STANDARD=98 -G Ninja ../..
+ninja
+
+echo "Done!"
