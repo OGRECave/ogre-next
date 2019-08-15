7d927903458cab07db8e149a2e1ac7a004b330f1
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2019-08-15 19:34:42 -0300

    Bugfix: Detail normal maps with gaps did not use the correct detail normal weights Thanks hedphelym for providing repro. See https://forums.ogre3d.org/viewtopic.php?f=25&p=545799#p545799

--- Components/Hlms/Pbs/src/OgreHlmsPbs.cpp
+++ Components/Hlms/Pbs/src/OgreHlmsPbs.cpp
@@ -641,19 +641,14 @@ namespace Ogre
             ++numNormalWeights;
         }
 
+        for( size_t i=0; i<4; ++i )
         {
-            size_t validDetailMaps = 0;
-            for( size_t i=0; i<4; ++i )
+            if( datablock->getTexture( PBSM_DETAIL0_NM + i ) )
             {
-                if( datablock->getTexture( PBSM_DETAIL0_NM + i ) )
+                if( datablock->getDetailNormalWeight( i ) != 1.0f )
                 {
-                    if( datablock->getDetailNormalWeight( i ) != 1.0f )
-                    {
-                        setProperty( *PbsProperty::DetailNormalWeights[validDetailMaps], 1 );
-                        ++numNormalWeights;
-                    }
-
-                    ++validDetailMaps;
+                    setProperty( *PbsProperty::DetailNormalWeights[i], 1 );
+                    ++numNormalWeights;
                 }
             }
         }
