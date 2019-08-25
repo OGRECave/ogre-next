df660d1268caf32ecfe483cbd0bc7b3d0aa128d4
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2019-08-25 02:38:55 -0300

    Added OpenVR sample. It's very WIP. OpenVR dependencies are hardcoded to my computer's path until the sample stops being so WIP.

---
+++ Samples/2.0/Tutorials/Tutorial_OpenVR/Tutorial_OpenVR.h
@@ -0,0 +1,58 @@
+
+#ifndef _Demo_Tutorial_OpenVR_H_
+#define _Demo_Tutorial_OpenVR_H_
+
+#include "GraphicsSystem.h"
+
+#if __cplusplus <= 199711L
+    #ifndef nullptr
+        #define OgreDemoNullptrDefined
+        #define nullptr (0)
+    #endif
+#endif
+#include "openvr.h"
+#if __cplusplus <= 199711L
+    #ifdef OgreDemoNullptrDefined
+        #undef OgreDemoNullptrDefined
+        #undef nullptr
+    #endif
+#endif
+
+
+namespace Demo
+{
+    class Tutorial_OpenVRGraphicsSystem : public GraphicsSystem
+    {
+        vr::IVRSystem *mHMD;
+        std::string mStrDriver;
+        std::string mStrDisplay;
+        vr::TrackedDevicePose_t mTrackedDevicePose[vr::k_unMaxTrackedDeviceCount];
+
+        Ogre::TextureGpu    *mVrTexture;
+
+        virtual Ogre::CompositorWorkspace* setupCompositor();
+
+        virtual void setupResources(void);
+
+        static std::string GetTrackedDeviceString( vr::TrackedDeviceIndex_t unDevice,
+                                                   vr::TrackedDeviceProperty prop,
+                                                   vr::TrackedPropertyError *peError = NULL );
+        void initOpenVR(void);
+        void initCompositorVR(void);
+
+    public:
+        Tutorial_OpenVRGraphicsSystem( GameState *gameState ) :
+            GraphicsSystem( gameState ),
+            mHMD( 0 ),
+            mVrTexture( 0 )
+        {
+            memset( mTrackedDevicePose, 0, sizeof (mTrackedDevicePose) );
+        }
+
+        virtual void deinitialize(void);
+
+        virtual void createScene01(void);
+    };
+}
+
+#endif
