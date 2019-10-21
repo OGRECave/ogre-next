0321173d65f930c30ef9ec7addb7f99a71971e04
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2019-10-21 16:16:52 -0300

    patch version++ Ogre 2.2.0 will soon be released, and Irradiance Fields with Depth will be an added feature in a future release
    --HG--
    branch : v2-2-irradiance-field

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -51,7 +51,7 @@ namespace Ogre {
     // Define ogre version
     #define OGRE_VERSION_MAJOR 2
     #define OGRE_VERSION_MINOR 2
-    #define OGRE_VERSION_PATCH 0
+    #define OGRE_VERSION_PATCH 1
     #define OGRE_VERSION_SUFFIX "unstable"
     #define OGRE_VERSION_NAME "Cerberus"
 
