OGRECave/ogre-next

commit 1c1bc38701f72c8f52f2baba75e7828a713d37e6

Renamed some variables, tweaked some padding Improved culling of per pixel PCC by tightening the AABB
--HG--
branch : v2-2-irradiance-field

Components/Hlms/Pbs/src/Cubemaps/OgreCubemapProbe.cpp
@@ -239,10 +239,16 @@ namespace Ogre
     {
         if( !mInternalProbe )
             return;
+
+        Vector3 restrictedAabbMin( mArea.getMinimum() );
+        Vector3 restrictedAabbMax( mArea.getMaximum() );
+        restrictedAabbMin.makeCeil( mProbeShape.getMinimum() );
+        restrictedAabbMax.makeCeil( mProbeShape.getMaximum() );
+
         Quaternion qRot( mOrientation );
         SceneNode *sceneNode = mInternalProbe->getParentSceneNode();
-        sceneNode->setPosition( mArea.mCenter );
-        sceneNode->setScale( mArea.mHalfSize * 2.0f );
+        sceneNode->setPosition( ( restrictedAabbMin + restrictedAabbMax ) * 0.5f );
+        sceneNode->setScale( restrictedAabbMax - restrictedAabbMin );
         sceneNode->setOrientation( qRot );
 
         mCreator->fillConstBufferData( *this, Matrix4::IDENTITY, Matrix3::IDENTITY,

Components/Hlms/Pbs/src/Cubemaps/OgrePccPerPixelGridPlacement.cpp
@@ -217,29 +217,30 @@ namespace Ogre
 
         const Vector3 camCenter( probe->getProbeCameraPos() );
         const Vector3 camCenterLS( probe->getInvOrientation() * ( camCenter - probeShapeCenter ) );
-        Vector3 probeAreaMax;
-        Vector3 probeAreaMin;
-        probeAreaMax.x = camCenterLS.x + ( mFullRegion.mHalfSize.x - camCenterLS.x ) *
-                                             colourVal[CubemapSide::PX].a * 2.0f;
-        probeAreaMin.x = camCenterLS.x + ( -mFullRegion.mHalfSize.x - camCenterLS.x ) *
-                                             colourVal[CubemapSide::NX].a * 2.0f;
-        probeAreaMax.y = camCenterLS.y + ( mFullRegion.mHalfSize.y - camCenterLS.y ) *
-                                             colourVal[CubemapSide::PY].a * 2.0f;
-        probeAreaMin.y = camCenterLS.y + ( -mFullRegion.mHalfSize.y - camCenterLS.y ) *
-                                             colourVal[CubemapSide::NY].a * 2.0f;
-        probeAreaMax.z = camCenterLS.z + ( mFullRegion.mHalfSize.z - camCenterLS.z ) *
-                                             colourVal[CubemapSide::PZ].a * 2.0f;
-        probeAreaMin.z = camCenterLS.z + ( -mFullRegion.mHalfSize.z - camCenterLS.z ) *
-                                             colourVal[CubemapSide::NZ].a * 2.0f;
-        probeAreaMin += probeShapeCenter;
-        probeAreaMax += probeShapeCenter;
+        Vector3 probeShapeMax;
+        Vector3 probeShapeMin;
+        probeShapeMax.x = camCenterLS.x + ( mFullRegion.mHalfSize.x - camCenterLS.x ) *
+                                              colourVal[CubemapSide::PX].a * 2.0f;
+        probeShapeMin.x = camCenterLS.x + ( -mFullRegion.mHalfSize.x - camCenterLS.x ) *
+                                              colourVal[CubemapSide::NX].a * 2.0f;
+        probeShapeMax.y = camCenterLS.y + ( mFullRegion.mHalfSize.y - camCenterLS.y ) *
+                                              colourVal[CubemapSide::PY].a * 2.0f;
+        probeShapeMin.y = camCenterLS.y + ( -mFullRegion.mHalfSize.y - camCenterLS.y ) *
+                                              colourVal[CubemapSide::NY].a * 2.0f;
+        probeShapeMax.z = camCenterLS.z + ( mFullRegion.mHalfSize.z - camCenterLS.z ) *
+                                              colourVal[CubemapSide::PZ].a * 2.0f;
+        probeShapeMin.z = camCenterLS.z + ( -mFullRegion.mHalfSize.z - camCenterLS.z ) *
+                                              colourVal[CubemapSide::NZ].a * 2.0f;
+        probeShapeMin *= 1.01f; // Padding
+        probeShapeMax *= 1.01f; // Padding
+        probeShapeMin += probeShapeCenter;
+        probeShapeMax += probeShapeCenter;
 
-        snapToFullRegion( probeAreaMin, probeAreaMax );
-        snapToSides( probeIdx, probeAreaMin, probeAreaMax );
+        snapToFullRegion( probeShapeMin, probeShapeMax );
+        snapToSides( probeIdx, probeShapeMin, probeShapeMax );
 
         Aabb probeShape;
-        probeShape.setExtents( probeAreaMin, probeAreaMax );
-        probeShape.mHalfSize *= 1.05f;
+        probeShape.setExtents( probeShapeMin, probeShapeMax );
         Aabb area( camCenter, probeAreaHalfSize );
         probe->set( camCenter, area, Vector3( 0.5f ), Matrix3::IDENTITY, probeShape );
     }

commit 2efefde4eceb061f2e4fa4eb2361c8a4e13110db

Bugfix: Wrong operation introduced in 1c1bc38701f72c8f52f2baba75e7828a713d37e6 caused some per pixel PCC probes to disappear
--HG--
branch : v2-2-irradiance-field

Components/Hlms/Pbs/src/Cubemaps/OgreCubemapProbe.cpp
@@ -244,7 +244,7 @@ namespace Ogre
         Vector3 restrictedAabbMin( mArea.getMinimum() );
         Vector3 restrictedAabbMax( mArea.getMaximum() );
         restrictedAabbMin.makeCeil( mProbeShape.getMinimum() );
-        restrictedAabbMax.makeCeil( mProbeShape.getMaximum() );
+        restrictedAabbMax.makeFloor( mProbeShape.getMaximum() );
 
         Quaternion qRot( mOrientation );
         SceneNode *sceneNode = mInternalProbe->getParentSceneNode();