6532b3ec1e0e602f553dfff025ef5f21f17b83f6
Eugene Golushkov <e.golushkov@nospam_gmail.com>
2020-01-14 19:26:29 +0200

    D3D11: new style FSAA modes enumeration, with "8x CSAA", "8x MSAA", "16xQ CSAA" values

--- RenderSystems/Direct3D11/src/OgreD3D11RenderSystem.cpp
+++ RenderSystems/Direct3D11/src/OgreD3D11RenderSystem.cpp
@@ -647,6 +647,8 @@ namespace Ogre
             // set maskable levels supported
             for (unsigned int n = 1; n <= D3D11_MAX_MULTISAMPLE_SAMPLE_COUNT; n++)
             {
+#if 0
+                // old style enumeration, with cryptic "8", "8 [Quality]" strings for 8xCSAA and 8xMSAA, where it is not even clear what "8" means
                 HRESULT hr = device->CheckMultisampleQualityLevels(format, n, &numLevels);
                 if (SUCCEEDED(hr) && numLevels > 0)
                 {
@@ -669,6 +671,17 @@ namespace Ogre
                     if(csaa16x && csaa16xQ)
                         optFSAA->possibleValues.push_back("16 [Quality]");
                 }
+#else
+                // new style enumeration, with "8x CSAA", "8x MSAA" values
+                if(n == 8 && SUCCEEDED(device->CheckMultisampleQualityLevels(format, 4, &numLevels)) && numLevels > 8)    // 8x CSAA
+                    optFSAA->possibleValues.push_back("8x CSAA");
+                if(n == 16 && SUCCEEDED(device->CheckMultisampleQualityLevels(format, 4, &numLevels)) && numLevels > 16)  // 16x CSAA
+                    optFSAA->possibleValues.push_back("16x CSAA");
+                if(n == 16 && SUCCEEDED(device->CheckMultisampleQualityLevels(format, 8, &numLevels)) && numLevels > 16)  // 16xQ CSAA
+                    optFSAA->possibleValues.push_back("16xQ CSAA");
+                if (SUCCEEDED(device->CheckMultisampleQualityLevels(format, n, &numLevels)) && numLevels > 0)             // Nx MSAA
+                    optFSAA->possibleValues.push_back(StringConverter::toString(n) + "x MSAA");
+#endif
             }
         }
 
@@ -3652,7 +3665,7 @@ namespace Ogre
         DXGI_FORMAT dxgiFormat = D3D11Mappings::get(format);
         unsigned samples = StringConverter::parseUnsignedInt(fsaa);
         bool msaaOnly = fsaa.find("MSAA") != String::npos;
-        bool qualityHint = samples >= 8 && fsaa.find("Quality") != String::npos;
+        bool qualityHint = samples >= 8 && fsaa.find("Quality") != String::npos || fsaa.find("16xQ") != String::npos;
 
         // NVIDIA, AMD - prefer CSAA aka EQAA if available.
         // see http://developer.download.nvidia.com/assets/gamedev/docs/CSAA_Tutorial.pdf
