451d41f82ed13f29662220c0a448d97f587f7c40
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2020-02-25 18:44:04 -0300

    Build fixes:  * Memory Tracker would cause compiler errors due to unordered_map issues. It was already broken before on Linux, due to a circular dependency (unordered_map needs allocators which needs the tracker, and the tracker needs unordered_map). So we use std::unordered_map directly instead of the Ogre overloaded one  * OGRE_BUILD_COMPONENT_PLANAR_REFLECTIONS would not compile
    --HG--
    branch : v2-2-build-time-opt

--- OgreMain/include/OgreMemoryTracker.h
+++ OgreMain/include/OgreMemoryTracker.h
@@ -63,7 +63,32 @@ namespace __gnu_cxx
 #endif
 
 #if OGRE_MEMORY_TRACKER
-#   include "Threading/OgreThreadHeaders.h"
+#    include "Threading/OgreThreadHeaders.h"
+
+#    if( OGRE_COMPILER == OGRE_COMPILER_GNUC ) && !defined( STLPORT )
+#        if __cplusplus >= 201103L
+#            include <unordered_map>
+#        elif OGRE_COMP_VER >= 430
+#            include <tr1/unordered_map>
+#        else
+#            include <ext/hash_map>
+#        endif
+#    elif( OGRE_COMPILER == OGRE_COMPILER_CLANG )
+#        if defined( _LIBCPP_VERSION ) || __cplusplus >= 201103L
+#            include <unordered_map>
+#        else
+#            include <tr1/unordered_map>
+#        endif
+#    elif !defined( STLPORT )
+#        if( OGRE_COMPILER == OGRE_COMPILER_MSVC ) && _MSC_FULL_VER >= 150030729  // VC++ 9.0 SP1+
+#            include <unordered_map>
+#        elif OGRE_THREAD_PROVIDER == 1
+#            include <boost/unordered_map.hpp>
+#        else
+#            error \
+                "Your compiler doesn't support unordered_map. Try to compile Ogre with Boost or STLPort."
+#        endif
+#    endif
 #endif
 
 namespace Ogre
@@ -109,7 +134,8 @@ namespace Ogre
 
         std::string mLeakFileName;
         bool mDumpToStdOut;
-        typedef unordered_map<void*, Alloc>::type AllocationMap;
+        // typedef std::unordered_map<void*, Alloc> AllocationMap;
+        typedef OGRE_HASH_NAMESPACE::OGRE_HASHMAP_NAME<void*, Alloc> AllocationMap;
         AllocationMap mAllocations;
 
         size_t mTotalAllocations;
