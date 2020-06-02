OGRECave/ogre-next

commit f87a1400253c17e66edeae92537690aca59f2f1a

Fixed crash when trying to use AbsolutePixelCountLodStrategy
AbsolutePixelCountLodStrategy needs a valid Viewport, however
LOD calculations happen before the Viewport is setup.

This caused a catch-22 problem because setRenderPassDescToCurrent setups
the Viewport, however it can't be called before calculating LODs, but
LODs need setRenderPassDescToCurrent to be called.

Solution was to manually setup just a single Viewport in the LOD camera
so that LOD calculations can proceed, since the full call to
setRenderPassDescToCurrent isn't actually needed

OgreMain/src/Compositor/Pass/PassScene/OgreCompositorPassScene.cpp
@@ -181,6 +181,15 @@ namespace Ogre
 
         notifyPassEarlyPreExecuteListeners();
 
+        SceneManager *sceneManager = mCamera->getSceneManager();
+
+        // Our LOD camera must have set a valid viewport. If usedLodCamera gets overriden by
+        // our input argument, then it's caller's responsibility to have set a valid viewport
+        Viewport *viewport = sceneManager->getCurrentViewport0();
+        viewport->_setVisibilityMask( mDefinition->mVisibilityMask, mDefinition->mLightVisibilityMask );
+        setViewportSizeToViewport( 0u, viewport );
+        mLodCamera->_notifyViewport( viewport );
+
         Camera const *usedLodCamera = mLodCamera;
         if( lodCamera && mDefinition->mLodCameraName == IdString() )
             usedLodCamera = lodCamera;
@@ -190,8 +199,6 @@ namespace Ogre
         String oldViewportMatScheme = mViewport->getMaterialScheme();
         mViewport->setMaterialScheme(mDefinition->mMaterialScheme);
 #endif
-
-        SceneManager *sceneManager = mCamera->getSceneManager();
 
         const Quaternion oldCameraOrientation( mCamera->getOrientation() );
 
@@ -215,9 +222,6 @@ namespace Ogre
             sceneManager->_setCurrentShadowNode( shadowNode, mDefinition->mShadowNodeRecalculation ==
                                                                                     SHADOW_NODE_REUSE );
         }
-
-        Viewport *viewport = sceneManager->getCurrentViewport0();
-        viewport->_setVisibilityMask( mDefinition->mVisibilityMask, mDefinition->mLightVisibilityMask );
 
         //Fire the listener in case it wants to change anything
         notifyPassPreExecuteListeners();

OgreMain/include/Compositor/Pass/OgreCompositorPass.h
@@ -135,6 +135,16 @@ namespace Ogre
         /// in case derived class wants to make some changes.
         virtual void postRenderPassDescriptorSetup( RenderPassDescriptor *renderPassDesc ) {}
 
+        /// Does the same as setRenderPassDescToCurrent, but only setting a single Viewport.
+        /// (setRenderPassDescToCurrent does much more)
+        ///
+        /// Needed due to catch-22 problem:
+        ///     1. Some algorithms (like LOD) need a Viewport set
+        ///     2. We can't call setRenderPassDescToCurrent until that algorithm has run
+        ///     3. The algorithm thus can't run if setRenderPassDescToCurrent isn't set
+        ///
+        /// See https://forums.ogre3d.org/viewtopic.php?p=548046#p548046
+        void setViewportSizeToViewport( size_t vpIdx, Viewport *outVp );
         void setRenderPassDescToCurrent(void);
 
         void populateTextureDependenciesFromExposedTextures(void);

OgreMain/src/Compositor/Pass/OgreCompositorPass.cpp
@@ -118,6 +118,38 @@ namespace Ogre
         populateTextureDependenciesFromExposedTextures();
     }
     //-----------------------------------------------------------------------------------
+    void CompositorPass::setViewportSizeToViewport( size_t vpIdx, Viewport *outVp )
+    {
+        if( mDefinition->mNumViewports == 0u )
+            return;
+
+        CompositorWorkspace *workspace = mParentNode->getWorkspace();
+        uint8 workspaceVpMask = workspace->getViewportModifierMask();
+
+        bool applyModifier = ( workspaceVpMask & mDefinition->mViewportModifierMask ) != 0;
+        Vector4 vpModifier = applyModifier ? workspace->getViewportModifier() : Vector4( 0, 0, 1, 1 );
+
+        vpIdx = std::min<size_t>( vpIdx, mDefinition->mNumViewports - 1u );
+
+        Vector4 vpSize;
+        Vector4 scissors;
+
+        const Real left = mDefinition->mVpRect[vpIdx].mVpLeft + vpModifier.x;
+        const Real top = mDefinition->mVpRect[vpIdx].mVpTop + vpModifier.y;
+        const Real width = mDefinition->mVpRect[vpIdx].mVpWidth * vpModifier.z;
+        const Real height = mDefinition->mVpRect[vpIdx].mVpHeight * vpModifier.w;
+
+        const Real scLeft = mDefinition->mVpRect[vpIdx].mVpScissorLeft + vpModifier.x;
+        const Real scTop = mDefinition->mVpRect[vpIdx].mVpScissorTop + vpModifier.y;
+        const Real scWidth = mDefinition->mVpRect[vpIdx].mVpScissorWidth * vpModifier.z;
+        const Real scHeight = mDefinition->mVpRect[vpIdx].mVpScissorHeight * vpModifier.w;
+
+        vpSize = Vector4( left, top, width, height );
+        scissors = Vector4( scLeft, scTop, scWidth, scHeight );
+
+        outVp->setDimensions( mAnyTargetTexture, vpSize, scissors, mAnyMipLevel );
+    }
+    //-----------------------------------------------------------------------------------
     void CompositorPass::setRenderPassDescToCurrent(void)
     {
         CompositorWorkspace *workspace = mParentNode->getWorkspace();

commit 2bc144d3c2c76163211223ac5120008f5062c786

Fixed regression which breaks D3D11 and possibly other RS
Regression introduced in f87a1400253c17e66edeae92537690aca59f2f1a

OgreMain/src/Compositor/Pass/PassScene/OgreCompositorPassScene.cpp
@@ -183,13 +183,6 @@ namespace Ogre
 
         SceneManager *sceneManager = mCamera->getSceneManager();
 
-        // Our LOD camera must have set a valid viewport. If usedLodCamera gets overriden by
-        // our input argument, then it's caller's responsibility to have set a valid viewport
-        Viewport *viewport = sceneManager->getCurrentViewport0();
-        viewport->_setVisibilityMask( mDefinition->mVisibilityMask, mDefinition->mLightVisibilityMask );
-        setViewportSizeToViewport( 0u, viewport );
-        mLodCamera->_notifyViewport( viewport );
-
         Camera const *usedLodCamera = mLodCamera;
         if( lodCamera && mDefinition->mLodCameraName == IdString() )
             usedLodCamera = lodCamera;
@@ -209,10 +202,25 @@ namespace Ogre
             mCamera->setOrientation( oldCameraOrientation * CubemapRotations[sliceIdx] );
         }
 
+        Viewport *viewport = sceneManager->getCurrentViewport0();
+        viewport->_setVisibilityMask( mDefinition->mVisibilityMask, mDefinition->mLightVisibilityMask );
+
         if( mDefinition->mUpdateLodLists )
         {
-            sceneManager->updateAllLods( usedLodCamera, mDefinition->mLodBias,
+            // LODs may require up to date viewports. But we can't write to the viewport's dimensions
+            // without messing up beginRenderPassDescriptor internal state, so we use a local copy.
+            //
+            // Our LOD camera must have set a valid viewport. If usedLodCamera != mLodCamera due to
+            // our input argument, then it's caller's responsibility to have set a valid viewport
+            Viewport localVp = *viewport;
+            setViewportSizeToViewport( 0u, &localVp );
+            mLodCamera->_notifyViewport( &localVp );
+
+            sceneManager->updateAllLods( usedLodCamera, mDefinition->mLodBias,  //
                                          mDefinition->mFirstRQ, mDefinition->mLastRQ );
+
+            // Restore viewport
+            mLodCamera->_notifyViewport( viewport );
         }
 
         //Passes belonging to a ShadowNode should not override their parent.