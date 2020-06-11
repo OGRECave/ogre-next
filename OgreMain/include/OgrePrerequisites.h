46bac1ac7e6e53f0b867115faed492526142942a
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2020-06-11 16:34:43 -0300

    Bump version to 2.2.3, add unstable tag again

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -66,8 +66,8 @@ namespace Ogre {
     // Define ogre version
     #define OGRE_VERSION_MAJOR 2
     #define OGRE_VERSION_MINOR 2
-    #define OGRE_VERSION_PATCH 2
-    #define OGRE_VERSION_SUFFIX ""
+    #define OGRE_VERSION_PATCH 3
+    #define OGRE_VERSION_SUFFIX "unstable"
     #define OGRE_VERSION_NAME "Cerberus"
 
     #define OGRE_VERSION    ((OGRE_VERSION_MAJOR << 16) | (OGRE_VERSION_MINOR << 8) | OGRE_VERSION_PATCH)
