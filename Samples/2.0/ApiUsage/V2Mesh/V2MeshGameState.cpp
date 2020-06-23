OGRECave/ogre-next

commit 46692e77e5026f174752ddc617c4f05209f50e59

Prefer MeshManager::createByImportingV1() rather than Mesh::importV1() to keep mesh reloadable on device lost event (all other samples)

Samples/2.0/ApiUsage/V2Mesh/V2MeshGameState.cpp
@@ -40,16 +40,15 @@ namespace Demo
                     "athene.mesh", Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME,
                     Ogre::v1::HardwareBuffer::HBU_STATIC, Ogre::v1::HardwareBuffer::HBU_STATIC );
 
-        //Create a v2 mesh to import to, with a different name (arbitrary).
-        v2Mesh = Ogre::MeshManager::getSingleton().createManual(
-                    "athene.mesh Imported", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
         bool halfPosition   = true;
         bool halfUVs        = true;
         bool useQtangents   = true;
 
+        //Create a v2 mesh to import to, with a different name (arbitrary).
         //Import the v1 mesh to v2
-        v2Mesh->importV1( v1Mesh.get(), halfPosition, halfUVs, useQtangents );
+        v2Mesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "athene.mesh Imported", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    v1Mesh.get(), halfPosition, halfUVs, useQtangents );
 
         //We don't need the v1 mesh. Free CPU memory, get it out of the GPU.
         //Leave it loaded if you want to use athene with v1 Entity.
@@ -78,10 +77,9 @@ namespace Demo
                     "Barrel.mesh", Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME,
                     Ogre::v1::HardwareBuffer::HBU_STATIC, Ogre::v1::HardwareBuffer::HBU_STATIC );
         //Create a v2 mesh to import to, with a different name.
-        v2Mesh = Ogre::MeshManager::getSingleton().createManual(
-                    "Barrel Imported", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
-        v2Mesh->importV1( v1Mesh.get(), halfPosition, halfUVs, useQtangents );
+        v2Mesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "Barrel Imported", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    v1Mesh.get(), halfPosition, halfUVs, useQtangents );
         v1Mesh->unload();
 
         //Save the v2 mesh to disk (optional)

OgreMain/include/OgreMesh2.h
@@ -402,7 +402,8 @@ namespace Ogre {
         const LodValueArray* _getLodValueArray(void) const                      { return &mLodValues; }
 
         /** Imports a v1 mesh to this mesh, with optional optimization conversions.
-            This mesh must be in unloaded state.
+            This mesh must be in unloaded state. Resulting mesh would be non-reloadable, use
+            MeshManager::createByImportingV1 to create mesh that will survive device lost event.
         @remarks
             The vertex stream will be converted to a single interleaved buffer; i.e.
             if the original mesh had 3 vertex buffers:

Samples/2.0/ApiUsage/AnimationTagPoint/AnimationTagPointGameState.cpp
@@ -40,15 +40,14 @@ namespace Demo
                     "Stickman.mesh", Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME,
                     Ogre::v1::HardwareBuffer::HBU_STATIC, Ogre::v1::HardwareBuffer::HBU_STATIC );
 
-        v2Mesh = Ogre::MeshManager::getSingleton().createManual(
-                    "Stickman.mesh", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
         bool halfPosition   = true;
         bool halfUVs        = true;
         bool useQtangents   = false;
 
         //Import the v1 mesh to v2
-        v2Mesh->importV1( v1Mesh.get(), halfPosition, halfUVs, useQtangents );
+        v2Mesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "Stickman.mesh", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    v1Mesh.get(), halfPosition, halfUVs, useQtangents );
 
         //We don't need the v1 mesh. Free CPU memory, get it out of the GPU.
         //Leave it loaded if you want to use athene with v1 Entity.

Samples/2.0/ApiUsage/AreaApproxLights/AreaApproxLightsGameState.cpp
@@ -153,11 +153,11 @@ namespace Demo
                                             1, 1, true, 1, 1.0f, 1.0f, Ogre::Vector3::UNIT_Y,
                                             Ogre::v1::HardwareBuffer::HBU_STATIC,
                                             Ogre::v1::HardwareBuffer::HBU_STATIC );
-        Ogre::MeshPtr lightPlaneMesh = Ogre::MeshManager::getSingleton().createManual(
-                    "LightPlane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-        lightPlaneMesh->importV1( lightPlaneMeshV1.get(), true, true, true );
+        Ogre::MeshPtr lightPlaneMesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "LightPlane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    lightPlaneMeshV1.get(), true, true, true );
 
-        Ogre::v1::MeshManager::getSingleton().remove( lightPlaneMeshV1 );
+        lightPlaneMeshV1->unload();
     }
     //-----------------------------------------------------------------------------------
     Ogre::HlmsUnlitDatablock* AreaApproxLightsGameState::createPlaneForAreaLight( Ogre::Light *light )
@@ -233,10 +233,9 @@ namespace Demo
                                             Ogre::v1::HardwareBuffer::HBU_STATIC,
                                             Ogre::v1::HardwareBuffer::HBU_STATIC );
 
-        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createManual(
-                    "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
-        planeMesh->importV1( planeMeshV1.get(), true, true, true );
+        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    planeMeshV1.get(), true, true, true );
 
         {
             Ogre::Item *item = sceneManager->createItem( planeMesh, Ogre::SCENE_DYNAMIC );

Samples/2.0/ApiUsage/Decals/DecalsGameState.cpp
@@ -50,11 +50,10 @@ namespace Demo
                                             Ogre::v1::HardwareBuffer::HBU_STATIC,
                                             Ogre::v1::HardwareBuffer::HBU_STATIC );
 
-        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createManual(
-                    "DebugDecalPlane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
-        planeMesh->importV1( planeMeshV1.get(), true, true, false );
-        Ogre::v1::MeshManager::getSingleton().remove( planeMeshV1 );
+        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "DebugDecalPlane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    planeMeshV1.get(), true, true, false );
+        planeMeshV1->unload();
 
         Ogre::HlmsManager *hlmsManager = mGraphicsSystem->getRoot()->getHlmsManager();
         Ogre::Hlms *hlmsUnlit = hlmsManager->getHlms( Ogre::HLMS_UNLIT );
@@ -184,10 +183,9 @@ namespace Demo
                                             Ogre::v1::HardwareBuffer::HBU_STATIC,
                                             Ogre::v1::HardwareBuffer::HBU_STATIC );
 
-        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createManual(
-                    "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
-        planeMesh->importV1( planeMeshV1.get(), true, true, true );
+        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    planeMeshV1.get(), true, true, true );
 
         {
             Ogre::Item *item = sceneManager->createItem( planeMesh, Ogre::SCENE_DYNAMIC );

Samples/2.0/ApiUsage/IesProfiles/IesProfilesGameState.cpp
@@ -53,10 +53,9 @@ namespace Demo
             Ogre::Vector3::UNIT_Z, Ogre::v1::HardwareBuffer::HBU_STATIC,
             Ogre::v1::HardwareBuffer::HBU_STATIC );
 
-        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createManual(
-            "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
-        planeMesh->importV1( planeMeshV1.get(), true, true, true );
+        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+            "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+            planeMeshV1.get(), true, true, true );
 
         {
             // Floor

Samples/2.0/ApiUsage/ImportAnimationsShareSkeletonInstance/ImportAnimationsShareSkeletonInstanceGameState.cpp
@@ -41,9 +41,9 @@ namespace Demo
             v1Mesh = Ogre::v1::MeshManager::getSingleton().load(
                 "char_reference.mesh", Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME,
                 Ogre::v1::HardwareBuffer::HBU_STATIC, Ogre::v1::HardwareBuffer::HBU_STATIC);
-            v2Mesh = Ogre::MeshManager::getSingleton().createManual(
-                "char_reference.mesh", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-            v2Mesh->importV1(v1Mesh.get(), halfPosition, halfUVs, useQtangents);
+            v2Mesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                "char_reference.mesh", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                v1Mesh.get(), halfPosition, halfUVs, useQtangents);
             v1Mesh->unload();
         }
 
@@ -59,9 +59,9 @@ namespace Demo
             v1Mesh = Ogre::v1::MeshManager::getSingleton().load(
                         parts[i], Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME,
                         Ogre::v1::HardwareBuffer::HBU_STATIC, Ogre::v1::HardwareBuffer::HBU_STATIC );
-            v2Mesh = Ogre::MeshManager::getSingleton().createManual(
-                        parts[i], Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-            v2Mesh->importV1( v1Mesh.get(), halfPosition, halfUVs, useQtangents );
+            v2Mesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                        parts[i], Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                        v1Mesh.get(), halfPosition, halfUVs, useQtangents );
             v1Mesh->unload();
         }
 

Samples/2.0/ApiUsage/InstancedStereo/InstancedStereoGameState.cpp
@@ -81,10 +81,9 @@ namespace Demo
                                             Ogre::v1::HardwareBuffer::HBU_STATIC,
                                             Ogre::v1::HardwareBuffer::HBU_STATIC );
 
-        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createManual(
-                    "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME );
-
-        planeMesh->importV1( planeMeshV1.get(), true, true, true );
+        Ogre::MeshPtr planeMesh = Ogre::MeshManager::getSingleton().createByImportingV1(
+                    "Plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+                    planeMeshV1.get(), true, true, true );
 
         {
             Ogre::Item *item = sceneManager->createItem( planeMesh, Ogre::SCENE_DYNAMIC );

commit 2dcab1a29414bf53fe25d0b8dad97715064fb07b

Fix regression crash in V2Mesh (#105)
Regression in 46692e77e5026f174752ddc617c4f05209f50e59
Fixes #105

Samples/2.0/ApiUsage/V2Mesh/V2MeshGameState.cpp
@@ -80,6 +80,7 @@ namespace Demo
         v2Mesh = Ogre::MeshManager::getSingleton().createByImportingV1(
                     "Barrel Imported", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
                     v1Mesh.get(), halfPosition, halfUVs, useQtangents );
+        v2Mesh->load();
         v1Mesh->unload();
 
         //Save the v2 mesh to disk (optional)