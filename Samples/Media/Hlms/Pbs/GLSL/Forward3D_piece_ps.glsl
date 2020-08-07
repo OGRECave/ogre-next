5ca9f570b629ea281aaeafabbd8147a62f6d7e84
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2020-08-06 21:38:39 -0300

    Decals are working in Vulkan

--- Samples/Media/Hlms/Pbs/GLSL/Forward3D_piece_ps.glsl
+++ Samples/Media/Hlms/Pbs/GLSL/Forward3D_piece_ps.glsl
@@ -2,13 +2,16 @@
 
 @property( hlms_enable_decals )
 	@piece( DeclDecalsSamplers )
-		@property( hlms_decals_diffuse )uniform sampler2DArray decalsDiffuseTex;@end
-		@property( hlms_decals_normals )uniform sampler2DArray decalsNormalsTex;@end
+		@property( syntax == glslvk )
+			layout( ogre_s@value(decalsSampler) ) uniform sampler decalsSampler;
+		@end
+		@property( hlms_decals_diffuse )vulkan_layout( ogre_t@value(decalsDiffuseTex) ) uniform texture2DArray decalsDiffuseTex;@end
+		@property( hlms_decals_normals )vulkan_layout( ogre_t@value(decalsNormalsTex) ) uniform texture2DArray decalsNormalsTex;@end
 		@property( hlms_decals_diffuse == hlms_decals_emissive )
 			#define decalsEmissiveTex decalsDiffuseTex
 		@end
 		@property( hlms_decals_emissive && hlms_decals_diffuse != hlms_decals_emissive )
-			uniform sampler2DArray decalsEmissiveTex;
+			vulkan_layout( ogre_t@value(decalsEmissiveTex) ) uniform texture2DArray decalsEmissiveTex;
 		@end
 	@end
 @end
