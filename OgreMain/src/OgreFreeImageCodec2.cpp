OGRECave/ogre-next

commit 647de0359439eb4c2be91d142e81ee9c113515b4

load BGRA8 images as PFG_BGRA8_UNORM in FreeImageCodec2::decode

OgreMain/src/OgreFreeImageCodec2.cpp
@@ -542,18 +542,18 @@ namespace Ogre {
             case 24:
 #if FREEIMAGE_COLORORDER == FREEIMAGE_COLORORDER_RGB
                 origFormat = PFG_RGB8_UNORM;
+                supportedFormat = PFG_RGBA8_UNORM;
 #else
                 origFormat = PFG_BGR8_UNORM;
+                supportedFormat = PFG_BGRA8_UNORM;
 #endif
-                supportedFormat = PFG_RGBA8_UNORM;
                 break;
             case 32:
 #if FREEIMAGE_COLORORDER == FREEIMAGE_COLORORDER_RGB
-                origFormat = PFG_RGBA8_UNORM;
-#else
-                origFormat = PFG_BGRA8_UNORM;
-#endif
                 supportedFormat = PFG_RGBA8_UNORM;
+#else
+                supportedFormat = PFG_BGRA8_UNORM;
+#endif
                 break;
             };
             break;

commit f742dac71a46b174cfc042f41e7dacc963b85247

Fix corrupted textures in TextureBaking & UpdatingDecalsAndAreaLightTex
Regression started in 647de0359439eb4c2be91d142e81ee9c113515b4

PFG_BGRA8_UNORM format is not a normal format and should be avoided as
much as possible when being used in TextureGpu

OgreMain/src/OgreFreeImageCodec2.cpp
@@ -549,21 +549,25 @@ namespace Ogre {
                 break;
             case 24:
 #if FREEIMAGE_COLORORDER == FREEIMAGE_COLORORDER_RGB
-                origFormat = PFG_RGB8_UNORM;
+                origFormat = PFG_BGR8_UNORM;
                 supportedFormat = PFG_RGBA8_UNORM;
 #else
                 origFormat = PFG_BGR8_UNORM;
-                supportedFormat = PFG_BGRA8_UNORM;
+                // Do NOT use PFG_BGRA8_UNORM. That format MUST be avoided
+                supportedFormat = PFG_RGBA8_UNORM;
 #endif
                 break;
             case 32:
 #if FREEIMAGE_COLORORDER == FREEIMAGE_COLORORDER_RGB
+                origFormat = PFG_RGBA8_UNORM;
                 supportedFormat = PFG_RGBA8_UNORM;
 #else
-                supportedFormat = PFG_BGRA8_UNORM;
+                origFormat = PFG_BGRA8_UNORM;
+                // Do NOT use PFG_BGRA8_UNORM. That format MUST be avoided
+                supportedFormat = PFG_RGBA8_UNORM;
 #endif
                 break;
-            };
+            }
             break;
         case FIT_UINT16:
             // 16-bit greyscale