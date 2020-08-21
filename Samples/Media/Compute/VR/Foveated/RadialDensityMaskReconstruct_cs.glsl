4e488132bdabf75ee501d68275f92e94fdab4107
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2020-08-21 20:15:48 -0300

    Ported multiple compute shaders to Vulkan v2

--- Samples/Media/Compute/VR/Foveated/RadialDensityMaskReconstruct_cs.glsl
+++ Samples/Media/Compute/VR/Foveated/RadialDensityMaskReconstruct_cs.glsl
@@ -8,7 +8,7 @@
 #define ushort2 uvec2
 #define OGRE_imageWrite2D4( outImage, iuv, value ) imageStore( outImage, int2( iuv ), value )
 
-layout( vulkan( ogre_u0 ) vk_comma  @insertpiece(uav0_pf_type) )
+layout( vulkan( ogre_u0 ) vk_comma @insertpiece(uav0_pf_type) )
 uniform restrict writeonly @insertpiece(uav0_data_type) dstTex;
 
 vulkan_layout( ogre_t0 ) uniform texture2D srcTex;
