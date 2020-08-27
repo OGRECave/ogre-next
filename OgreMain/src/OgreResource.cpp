4bc7307bcd277b18e6e171c262120d5f5a24c26d
Eugene Golushkov <e.golushkov@nospam_gmail.com>
2020-08-27 11:10:08 +0300

    Properly reload Meshes v2 on device lost - most of the code is not ready to see resource in LOADSTATE_UNLOADED_MARKED_FOR_RELOAD

--- OgreMain/src/OgreResource.cpp
+++ OgreMain/src/OgreResource.cpp
@@ -348,10 +348,14 @@ namespace Ogre
     void Resource::reload(LoadingFlags flags)
     { 
             OGRE_LOCK_AUTO_MUTEX;
-        if (mLoadingState.get() == (flags & Resource::LF_MARKED_FOR_RELOAD ?
+        LoadingState old = mLoadingState.get();
+        if (old == (flags & Resource::LF_MARKED_FOR_RELOAD ?
             LOADSTATE_UNLOADED_MARKED_FOR_RELOAD : LOADSTATE_LOADED))
         {
-            unload();
+            if(old == LOADSTATE_UNLOADED_MARKED_FOR_RELOAD)
+                mLoadingState.set(LOADSTATE_UNLOADED);
+            else
+                unload();
             load();
         }
     }
