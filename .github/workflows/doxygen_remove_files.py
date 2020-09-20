82c2bf139c61b0b55263e220c054e117c92272cc
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2020-09-20 20:37:57 -0300

    Do not push the whole repo to gh-pages

---
+++ .github/workflows/doxygen_remove_files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+
+keep_files = [
+	'api',
+	'images',
+	'javascripts',
+	'stylesheets',
+	'vtests',
+	'.git',
+	'_config.yml',
+	'index.md',
+	'ogre-logo.png',
+	'.nojekyll',
+]
+
+for root, dirs, filenames in os.walk( '../../' ):
+	for fileName in filenames:
+		if fileName not in keep_files:
+			toRemove = os.path.join( root, fileName )
+			print( 'Removing ' + toRemove )
+	for dir in dirs:
+		if dir not in keep_files:
+			toRemove = os.path.join( root, dir )
+			print( 'Removing ' + toRemove )
+			shutil.rmtree( toRemove )
+	break
