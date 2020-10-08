OGRECave/ogre-next

commit 1e8373222d2a2f5979a29854fe544f5b8650fb80

Added ReadOnly buffers
They work like either TexBuffers or UavBuffers depending on support

GL3+ Preprocessor expanded to support:
    #define my_func( myArg ) prefix_##myArg
    my_func( a )
which will expand to:
    prefix_##myArg
It's not perfect (i.e. it will just remove all '#' in a macro expansion)
but it gets the job done

Hlms' RenderSystem-specific properties broke Hlms caches if used on a
GPU with different features

RenderSystems/GL3Plus/include/GLSL/OgreGLSLPreprocessor.h
@@ -211,6 +211,7 @@ namespace Ogre {
         int Line;
         /// True if we are at beginning of line
         bool BOL;
+        bool SupplimentaryExpand;
         /// A stack of 32 booleans packed into one value :)
         unsigned EnableOutput;
         /// The list of macros defined so far

RenderSystems/GL3Plus/src/GLSL/OgreGLSLPreprocessor.cpp
@@ -212,6 +212,7 @@ namespace Ogre {
             cpp.Define (Args [i].String, Args [i].Length, "", 0);
 
         // Now run the macro expansion through the supplimentary preprocessor
+        cpp.SupplimentaryExpand = true;
         Token xt = cpp.Parse (Value);
 
         Expanding = false;
@@ -250,6 +251,7 @@ namespace Ogre {
         EnableOutput = 1;
         Line = iLine;
         BOL = true;
+        SupplimentaryExpand = false;
     }
 
     CPreprocessor::~CPreprocessor ()
@@ -1386,7 +1388,10 @@ namespace Ogre {
                 // Backslash-Newline sequences are deleted, no matter where.
                 empty_lines++;
                 break;
-
+            case Token::TK_PUNCTUATION:
+                if (output_enabled && (t.String[0] != '#' || !SupplimentaryExpand))
+                    output.Append (t);
+                break;
             case Token::TK_NEWLINE:
                 if (empty_lines)
                 {

commit cc46b4728270bfc9fda8f14823d97ebd49ba6281

[GL3+] Fix rendering errors due to shaders failing to link
Our CPreprocessor has 2 constructors, and one of them was not
initializing all variables, causing uninitialized variables errors
This would be only picked up by MSVC 2019 in Debug builds.

Regression started in 1e8373222d2a2f5979a29854fe544f5b8650fb80

MICROCODE CACHE MUST BE MANUALLY INVALIDATED!
The cache won't pick up this fix, thus it must be manually deleted.
Otherwise the bug will remain.

See https://forums.ogre3d.org/viewtopic.php?f=25&p=549454

RenderSystems/GL3Plus/include/GLSL/OgreGLSLPreprocessor.h
@@ -443,8 +443,7 @@ namespace Ogre {
 
     public:
         /// Create an empty preprocessor object
-        CPreprocessor () : MacroList (NULL)
-        { }
+        CPreprocessor ();
 
         /// Destroy the preprocessor object
         virtual ~CPreprocessor ();

RenderSystems/GL3Plus/src/GLSL/OgreGLSLPreprocessor.cpp
@@ -243,6 +243,15 @@ namespace Ogre {
 
     CPreprocessor::ErrorHandlerFunc CPreprocessor::ErrorHandler = DefaultError;
 
+    CPreprocessor::CPreprocessor() : MacroList( NULL )
+    {
+        Source = 0;
+        SourceEnd = 0;
+        EnableOutput = 1;
+        Line = 0;
+        BOL = true;
+        SupplimentaryExpand = false;
+    }
 
     CPreprocessor::CPreprocessor (const Token &iToken, int iLine) : MacroList (NULL)
     {