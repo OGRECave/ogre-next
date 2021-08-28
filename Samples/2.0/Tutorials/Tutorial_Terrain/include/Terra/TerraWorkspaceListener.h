c4631ee488960dda04a6e8bd26ca118dc63e55de
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2021-08-28 19:12:32 -0300

    Add MIT license to Terra sample

--- Samples/2.0/Tutorials/Tutorial_Terrain/include/Terra/TerraWorkspaceListener.h
+++ Samples/2.0/Tutorials/Tutorial_Terrain/include/Terra/TerraWorkspaceListener.h
@@ -4,7 +4,7 @@ This source file is part of OGRE
     (Object-oriented Graphics Rendering Engine)
 For the latest info, see http://www.ogre3d.org/
 
-Copyright (c) 2000-2014 Torus Knot Software Ltd
+Copyright (c) 2000-2021 Torus Knot Software Ltd
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 -----------------------------------------------------------------------------
 */
+
 #ifndef _OgreTerraWorkspaceListener_H_
 #define _OgreTerraWorkspaceListener_H_
 
