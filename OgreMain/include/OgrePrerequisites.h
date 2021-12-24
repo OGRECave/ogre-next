90bdb05083921c77be1a7b8cf9180eee9a664388
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2021-12-23 22:16:24 -0300

    Incremented version to 2.4.0
    Change codename to E

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -64,10 +64,10 @@ THE SOFTWARE
 namespace Ogre {
     // Define ogre version
     #define OGRE_VERSION_MAJOR 2
-    #define OGRE_VERSION_MINOR 3
+    #define OGRE_VERSION_MINOR 4
     #define OGRE_VERSION_PATCH 0
     #define OGRE_VERSION_SUFFIX "unstable"
-    #define OGRE_VERSION_NAME "Daedalus"
+    #define OGRE_VERSION_NAME "E"
 
     #define OGRE_VERSION    ((OGRE_VERSION_MAJOR << 16) | (OGRE_VERSION_MINOR << 8) | OGRE_VERSION_PATCH)
 
