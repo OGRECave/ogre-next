2935db6f3c2a0188177f1e4943dfccefaca6d377
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-01-01 01:28:42 -0300

    Remove CMake search for OpenGL ES 1.x (#235)

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -127,10 +127,6 @@ find_package(OpenGL)
 macro_log_feature(OPENGL_FOUND "OpenGL 3+" "Support for the OpenGL 3+ render system" "http://www.opengl.org/" FALSE "" "")
 
 if(NOT OGRE_SKIP_GLES_SEARCHING)
-	# Find OpenGL ES 1.x
-	find_package(OpenGLES)
-	macro_log_feature(OPENGLES_FOUND "OpenGL ES 1.x" "Support for the OpenGL ES 1.x render system (DEPRECATED)" "http://www.khronos.org/opengles/" FALSE "" "")
-
 	# Find OpenGL ES 2.x
 	find_package(OpenGLES2)
 	macro_log_feature(OPENGLES2_FOUND "OpenGL ES 2.x" "Support for the OpenGL ES 2.x render system" "http://www.khronos.org/opengles/" FALSE "" "")
@@ -270,7 +266,6 @@ include_directories(
 
 link_directories(
   ${OPENGL_LIBRARY_DIRS}
-  ${OPENGLES_LIBRARY_DIRS}
   ${OPENGLES2_LIBRARY_DIRS}
   ${OPENGLES3_LIBRARY_DIRS}
   ${X11_LIBRARY_DIRS}
