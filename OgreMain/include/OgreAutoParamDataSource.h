f9a36091c064ebce5c664779bb04690602e49dba
James Walker <jw@jwwalker.com>
2022-03-18 13:28:53 -0700

    Fix Visual Studio alignment warning about AutoParamDataSource

--- OgreMain/include/OgreAutoParamDataSource.h
+++ OgreMain/include/OgreAutoParamDataSource.h
@@ -57,7 +57,30 @@ namespace Ogre
     */
     class _OgreExport AutoParamDataSource : public OgreAllocatedObj
     {
-    protected:
+    public:
+        void* operator new(size_t size) {
+            void *ptr = nullptr;
+            #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            ptr = _aligned_malloc( size, OGRE_SIMD_ALIGNMENT );
+            #else
+            // size must be an integral multiple of alignment
+            size = ( ( size + OGRE_SIMD_ALIGNMENT - 1 ) / OGRE_SIMD_ALIGNMENT ) * OGRE_SIMD_ALIGNMENT;
+            result = aligned_alloc( OGRE_SIMD_ALIGNMENT, size );
+            #endif
+            if (ptr == nullptr)
+            {
+                throw std::bad_alloc();
+            }
+            return ptr;
+        }
+        void operator delete(void* ptr) {
+            #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            _aligned_free( ptr );
+            #else
+            free( ptr );
+            #endif
+        }
+      protected:
         const Light &getLight( size_t index ) const;
         OGRE_SIMD_ALIGNED_DECL( mutable Matrix4, mWorldMatrix[256] );
         mutable size_t mWorldMatrixCount;
