e51e614cbe5c2b47110d4020c961f224fe73d182
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-07-26 20:04:10 -0300

    [GL] Fix shader compiler error on ancient drivers
    When GL_ARB_texture_buffer_range is not present and the shader uses
    bufferFetch1

--- Samples/Media/Hlms/Common/GLSL/CrossPlatformSettings_piece_all.glsl
+++ Samples/Media/Hlms/Common/GLSL/CrossPlatformSettings_piece_all.glsl
@@ -151,7 +151,9 @@
 
 #define OGRE_Load3D( tex, iuv, lod ) texelFetch( tex, ivec3( iuv ), lod )
 
-#define bufferFetch1( buffer, idx ) texelFetch( buffer, idx ).x
+@property( GL_ARB_texture_buffer_range )
+	#define bufferFetch1( buffer, idx ) texelFetch( buffer, idx ).x
+@end
 
 @property( syntax != glslvk )
 	#define OGRE_SAMPLER_ARG_DECL( samplerName )
@@ -206,24 +208,40 @@
 @property( !GL_ARB_texture_buffer_range || !GL_ARB_shading_language_420pack )
 @piece( SetCompatibilityLayer )
 	@property( !GL_ARB_texture_buffer_range )
-        #define samplerBuffer sampler2D
-        #define isamplerBuffer isampler2D
-        #define usamplerBuffer usampler2D
-        vec4 bufferFetch( in sampler2D sampl, in int pixelIdx )
-        {
-            ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
-            return texelFetch( sampl, pos, 0 );
-        }
-        ivec4 bufferFetch(in isampler2D sampl, in int pixelIdx)
-        {
-            ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
-            return texelFetch( sampl, pos, 0 );
-        }
-        uvec4 bufferFetch( in usampler2D sampl, in int pixelIdx )
-        {
-            ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
-            return texelFetch( sampl, pos, 0 );
-        }
+		#define samplerBuffer sampler2D
+		#define isamplerBuffer isampler2D
+		#define usamplerBuffer usampler2D
+		vec4 bufferFetch( in sampler2D sampl, in int pixelIdx )
+		{
+			ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
+			return texelFetch( sampl, pos, 0 );
+		}
+		ivec4 bufferFetch(in isampler2D sampl, in int pixelIdx)
+		{
+			ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
+			return texelFetch( sampl, pos, 0 );
+		}
+		uvec4 bufferFetch( in usampler2D sampl, in int pixelIdx )
+		{
+			ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
+			return texelFetch( sampl, pos, 0 );
+		}
+
+		float bufferFetch1( in sampler2D sampl, in int pixelIdx )
+		{
+			ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
+			return texelFetch( sampl, pos, 0 ).x;
+		}
+		int bufferFetch1(in isampler2D sampl, in int pixelIdx)
+		{
+			ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
+			return texelFetch( sampl, pos, 0 ).x;
+		}
+		uint bufferFetch1( in usampler2D sampl, in int pixelIdx )
+		{
+			ivec2 pos = ivec2( mod( pixelIdx, 2048 ), int( uint(pixelIdx) >> 11u ) );
+			return texelFetch( sampl, pos, 0 ).x;
+		}
 	@end
 @end
 @end
