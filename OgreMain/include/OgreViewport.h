OGRECave/ogre-next

commit a5b361891dc007980ce011c3e97e56e2ca460bc2

[Vk] Fix Planar Reflections missing barrier
PlanarReflections::cameraMatches would return inconsistent values
between analyzeBarriers and HlmsPbs::preparePassHash
Solution is to set Aspect Ratio earlier, *before* analyzeBarriers

OgreMain/include/OgreViewport.h
@@ -94,6 +94,8 @@ namespace Ogre
         */
         void _updateDimensions();
 
+        void _setupAspectRatio( Camera *cullCamera );
+
         /** Instructs the viewport to updates its contents.
          */
         void _updateCullPhase01( Camera *renderCamera, Camera *cullCamera, const Camera *lodCamera,

OgreMain/src/Compositor/Pass/PassScene/OgreCompositorPassScene.cpp
@@ -274,6 +274,16 @@ namespace Ogre
 
         notifyPassSceneAfterShadowMapsListeners();
 
+        {
+            // We must ensure Aspect Ratio is set BEFORE analyzeBarriers
+            // otherwise analyzeBarriers may make the wrong decisions
+            // (e.g. skipping barriers in planar reflections) because the AR
+            // may be used to determine whether a technique is used or not
+            setViewportSizeToViewport( 0u, viewport );
+            mCullCamera->_notifyViewport( viewport );
+            viewport->_setupAspectRatio( mCullCamera );
+        }
+
         analyzeBarriers();
         executeResourceTransitions();
         setRenderPassDescToCurrent();

OgreMain/src/OgreViewport.cpp
@@ -176,14 +176,17 @@ namespace Ogre
         _updateDimensions();
     }
     //---------------------------------------------------------------------
-    void Viewport::_updateCullPhase01( Camera *renderCamera, Camera *cullCamera, const Camera *lodCamera,
-                                       uint8 firstRq, uint8 lastRq, bool reuseCullData )
+    void Viewport::_setupAspectRatio( Camera *cullCamera )
     {
         // Automatic AR cameras are useful for cameras that draw into multiple viewports
         const Real aspectRatio = (Real)mActWidth / (Real)std::max( 1, mActHeight );
         if( cullCamera->getAutoAspectRatio() && cullCamera->getAspectRatio() != aspectRatio )
             cullCamera->setAspectRatio( aspectRatio );
-
+    }
+    //---------------------------------------------------------------------
+    void Viewport::_updateCullPhase01( Camera *renderCamera, Camera *cullCamera, const Camera *lodCamera,
+                                       uint8 firstRq, uint8 lastRq, bool reuseCullData )
+    {
 #if OGRE_NO_VIEWPORT_ORIENTATIONMODE == 0
         {
             const OrientationMode orientationMode = mCurrentTarget->getOrientationMode();

commit cee3a92db59f24c0e4fa611dc0d459b919afd3ff

[Regression] Fix D3D11 crash in Tutorial02 and simple scenes (#341)
Regression happened in a5b361891dc007980ce011c3e97e56e2ca460bc2

When we messed with the main Viewport to fix analyzeBarriers, we broke
D3D11's RenderSystem ability to detect whether the viewport has changed
since the last beginRenderPassDescriptor call.

Fixes #341

OgreMain/include/OgreViewport.h
@@ -176,6 +176,8 @@ namespace Ogre
 
         TextureGpu *getCurrentTarget() const { return mCurrentTarget; }
 
+        uint8 getCurrentMip() const { return mCurrentMip; }
+
         /** Only sets the scissor regions. The scissor rectangle must be fully inside
             the viewport rectangle. See setDimensions() for param description
         @remarks

OgreMain/src/Compositor/Pass/PassScene/OgreCompositorPassScene.cpp
@@ -274,6 +274,12 @@ namespace Ogre
 
         notifyPassSceneAfterShadowMapsListeners();
 
+        TextureGpu *oldTarget = viewport->getCurrentTarget();
+        const uint8 oldMip = viewport->getCurrentMip();
+        const Vector4 oldVpSize( viewport->getLeft(), viewport->getTop(), viewport->getWidth(),
+                                 viewport->getHeight() );
+        const Vector4 oldScissors( viewport->getScissorLeft(), viewport->getScissorTop(),
+                                   viewport->getScissorWidth(), viewport->getScissorHeight() );
         {
             // We must ensure Aspect Ratio is set BEFORE analyzeBarriers
             // otherwise analyzeBarriers may make the wrong decisions
@@ -286,6 +292,12 @@ namespace Ogre
 
         analyzeBarriers();
         executeResourceTransitions();
+        {
+            // Restore the old viewport settings because we messed up with
+            // RenderSystem::mCurrentRenderViewport (where viewport points to) and some
+            // RenderSystems (D3D11) tracks these settings to see which ones are dirty.
+            viewport->setDimensions( oldTarget, oldVpSize, oldScissors, oldMip );
+        }
         setRenderPassDescToCurrent();
 
         sceneManager->_setForwardPlusEnabledInPass( mDefinition->mEnableForwardPlus );