44d567bed1e0c69694181c315a69833d3e75ded8
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2023-08-11 18:40:18 -0300

    Fix crash

--- OgreMain/src/ParticleSystem/OgreParticleSystemManager2.cpp
+++ OgreMain/src/ParticleSystem/OgreParticleSystemManager2.cpp
@@ -216,9 +216,8 @@ void ParticleSystemManager2::updateSerialPre( const Real timeSinceLast )
             const size_t numSimdActiveParticles = systemDef->getNumSimdActiveParticles();
             if( numSimdActiveParticles > 0u )
             {
-                systemDef->mParticleGpuData =
-                    reinterpret_cast<ParticleGpuData *>( systemDef->mGpuData->map(
-                        0u, sizeof( ParticleGpuData ) * systemDef->getNumSimdActiveParticles() ) );
+                systemDef->mParticleGpuData = reinterpret_cast<ParticleGpuData *>(
+                    systemDef->mGpuData->map( 0u, sizeof( ParticleGpuData ) * numSimdActiveParticles ) );
             }
 
             systemDef->mVaoPerLod[0].back()->setPrimitiveRange(
