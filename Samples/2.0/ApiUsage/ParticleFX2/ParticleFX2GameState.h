10328cb3c8a5e2c93345b9f6e61ebe451ecef4be
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2023-09-01 12:34:20 -0300

    Add ParticleFX2 documentation
    Use Alpha Hashing + A2C in PFX2 sample.

--- Samples/2.0/ApiUsage/ParticleFX2/ParticleFX2GameState.h
+++ Samples/2.0/ApiUsage/ParticleFX2/ParticleFX2GameState.h
@@ -7,11 +7,29 @@
 
 namespace Demo
 {
+    namespace BlendingMethod
+    {
+        enum BlendingMethod
+        {
+            AlphaHashing,
+            AlphaHashingA2C,
+            AlphaBlending,
+            NumBlendingMethods
+        };
+    }
+
     class ParticleFX2GameState : public TutorialGameState
     {
         Ogre::SceneNode *mParticleSystem3_RootSceneNode;
         Ogre::SceneNode *mParticleSystem3_EmmitterSceneNode;
         float mTime;
+
+        BlendingMethod::BlendingMethod mCurrBlendingMethod;
+
+        void applyBlendingMethodToMaterials();
+        void applyBlendingMethodToMaterial( Ogre::HlmsDatablock *datablock );
+
+        void generateDebugText( float timeSinceLast, Ogre::String &outText ) override;
 
     public:
         ParticleFX2GameState( const Ogre::String &helpDescription );
@@ -19,6 +37,8 @@ namespace Demo
         void createScene01() override;
 
         void update( float timeSinceLast ) override;
+
+        void keyReleased( const SDL_KeyboardEvent &arg ) override;
     };
 }  // namespace Demo
 
