317eaca9d5a961132eeabbb662bbabf9d0bde839
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2023-11-10 20:17:19 -0300

    Use the old push method
    peaceiris/actions-gh-pages@v3 was ridiculously slow

--- Scripts/BuildScripts/build_ci_doxygen.sh
+++ Scripts/BuildScripts/build_ci_doxygen.sh
@@ -34,8 +34,19 @@ echo "--- Going to gh-pages repo ---"
 cd ../../gh-pages || exit $?
 cd api || exit $?
 echo "--- Removing old ${OGRE_VERSION} ---"
+git rm -rf ${OGRE_VERSION} || exit $?
 rm -rf ${OGRE_VERSION} || exit $?
 echo "--- Copying new ${OGRE_VERSION} ---"
 mv ../../build/Doxygen/api/html ${OGRE_VERSION} || exit $?
+
+git config user.email "darksylinc"
+git config user.name "darksylinc@users.noreply.github.com"
+
+echo "--- Adding to ${OGRE_VERSION} to git ---"
+git add ${OGRE_VERSION} || exit $?
+echo "--- Committing ---"
+git commit -m "Deploy GH" || exit $?
+echo "--- Pushing repo... ---"
+git push || exit $?
 
 echo "Done!"
