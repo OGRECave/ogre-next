bb07baeb2994d37f994df90b469be9b9a8d6e5bf
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2023-11-15 21:20:39 -0300

    [Vk/GL] Declare textures as mediump when appropriate
    We were doing this for Metal, but not for GLSL.

--- Samples/Media/Hlms/Pbs/GLSL/Forward3D_piece_ps.glsl
+++ Samples/Media/Hlms/Pbs/GLSL/Forward3D_piece_ps.glsl
@@ -5,13 +5,13 @@
 		@property( syntax == glslvk )
 			layout( ogre_s@value(decalsSampler) ) uniform sampler decalsSampler;
 		@end
-		@property( hlms_decals_diffuse )vulkan_layout( ogre_t@value(decalsDiffuseTex) ) uniform texture2DArray decalsDiffuseTex;@end
-		@property( hlms_decals_normals )vulkan_layout( ogre_t@value(decalsNormalsTex) ) uniform texture2DArray decalsNormalsTex;@end
+		@property( hlms_decals_diffuse )vulkan_layout( ogre_t@value(decalsDiffuseTex) ) midf_tex uniform texture2DArray decalsDiffuseTex;@end
+		@property( hlms_decals_normals )vulkan_layout( ogre_t@value(decalsNormalsTex) ) midf_tex uniform texture2DArray decalsNormalsTex;@end
 		@property( hlms_decals_diffuse == hlms_decals_emissive )
 			#define decalsEmissiveTex decalsDiffuseTex
 		@end
 		@property( hlms_decals_emissive && hlms_decals_diffuse != hlms_decals_emissive )
-			vulkan_layout( ogre_t@value(decalsEmissiveTex) ) uniform texture2DArray decalsEmissiveTex;
+			vulkan_layout( ogre_t@value(decalsEmissiveTex) ) midf_tex uniform texture2DArray decalsEmissiveTex;
 		@end
 	@end
 @end
