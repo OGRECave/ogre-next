cb42b179b924518819ad543c41304a0b4995f26a
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2023-12-05 20:42:18 -0300

    [Vk] Fix memory leak when using Low Level Materials with uniform params
    The param buffer was never flushed, never unmapped, never destroyed, and
    it kept growing slowly leaking RAM over time.
    
    Additionally fix validation errors of vkInvalidateMappedMemoryRanges due
    to incorrect handling of nonCoherentAtomSize.

--- RenderSystems/Vulkan/src/OgreVulkanQueue.cpp
+++ RenderSystems/Vulkan/src/OgreVulkanQueue.cpp
@@ -1185,6 +1185,8 @@ namespace Ogre
     {
         endCommandBuffer();
 
+        mRenderSystem->flushPendingNonCoherentFlushes( submissionType );
+
         // We must reset all bindings or else after 3 (mDynamicBufferCurrentFrame) frames
         // there could be dangling API handles left hanging around indefinitely that
         // may be collected by RootLayouts that use more slots than they need
