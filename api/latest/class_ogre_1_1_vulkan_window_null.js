16ed19f21f7c7eea7974380d011f3e5cbb18ea9b
darksylinc@users.noreply.github.com <darksylinc>
2024-03-21 15:13:16 +0000

    Deploy GH

--- api/latest/class_ogre_1_1_vulkan_window_null.js
+++ api/latest/class_ogre_1_1_vulkan_window_null.js
@@ -2,7 +2,7 @@ var class_ogre_1_1_vulkan_window_null =
 [
     [ "VulkanWindowNull", "class_ogre_1_1_vulkan_window_null.html#a00cc167571a7004b49c8244937810ed0", null ],
     [ "~VulkanWindowNull", "class_ogre_1_1_vulkan_window_null.html#a3e281b8e1b5fb583b81ee33c9d7b9360", null ],
-    [ "_initialize", "class_ogre_1_1_vulkan_window_null.html#a6b3888e69c1d17e76737d321d162a095", null ],
+    [ "_initialize", "class_ogre_1_1_vulkan_window_null.html#a1024a54c950b642cb205fc0faa56be22", null ],
     [ "_setVisible", "class_ogre_1_1_vulkan_window_null.html#ad0d1c111449872d8d3eda8ee72d664a2", null ],
     [ "destroy", "class_ogre_1_1_vulkan_window_null.html#aa49c7fb54f45b26ba2b7fce67c3c670b", null ],
     [ "isClosed", "class_ogre_1_1_vulkan_window_null.html#a4153ee89c55f636fad2db8695b597fdf", null ],
