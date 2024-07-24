7b19229b4cb51e11ae423bf78816acb8410ad91a
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2024-07-24 12:33:40 -0300

    [D3D11] Fix broken VEX_SPECULAR binding

--- RenderSystems/Direct3D11/src/OgreD3D11HLSLProgram.cpp
+++ RenderSystems/Direct3D11/src/OgreD3D11HLSLProgram.cpp
@@ -1966,7 +1966,7 @@ namespace Ogre {
                 {
                     inputDesc[currDesc].SemanticIndex = uvCount++;
                 }
-                else if( it->mSemantic == VES_DIFFUSE )
+                else if( it->mSemantic == VES_DIFFUSE || it->mSemantic == VES_SPECULAR )
                 {
                     inputDesc[currDesc].SemanticIndex = colourCount++;
                 }
