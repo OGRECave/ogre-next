OGRECave/ogre-next

commit 188068f5b64f6c699d76920d8642522051473bc6

[PFX2] Add shadow mapping support
Changed calculateHashForPreCaster signature

Components/Hlms/Pbs/src/OgreHlmsPbs.cpp
@@ -1053,7 +1053,8 @@ namespace Ogre
                                              const PiecesMap * )
     {
         HlmsPbsDatablock *datablock = static_cast<HlmsPbsDatablock *>( renderable->getDatablock() );
-        const bool hasAlphaTest = datablock->getAlphaTest() != CMPF_ALWAYS_PASS;
+        const bool hasAlphaTestOrHash =
+            datablock->getAlphaTest() != CMPF_ALWAYS_PASS || datablock->getAlphaHashing();
 
         HlmsPropertyVec::iterator itor = mT[kNoTid].setProperties.begin();
         HlmsPropertyVec::iterator endt = mT[kNoTid].setProperties.end();
@@ -1073,9 +1074,7 @@ namespace Ogre
                      itor->keyName != HlmsBaseProp::PoseNormals &&
                      itor->keyName != HlmsBaseProp::BonesPerVertex &&
                      itor->keyName != HlmsBaseProp::DualParaboloidMapping &&
-                     itor->keyName != HlmsBaseProp::AlphaTest &&
-                     itor->keyName != HlmsBaseProp::AlphaBlend &&
-                     ( !hasAlphaTest || !requiredPropertyByAlphaTest( itor->keyName ) ) )
+                     ( !hasAlphaTestOrHash || !requiredPropertyByAlphaTest( itor->keyName ) ) )
             {
                 itor = mT[kNoTid].setProperties.erase( itor );
                 endt = mT[kNoTid].setProperties.end();
@@ -1086,7 +1085,9 @@ namespace Ogre
             }
         }
 
-        if( hasAlphaTest && getProperty( kNoTid, PbsProperty::UseTextureAlpha ) )
+        setupSharedBasicProperties( renderable );
+
+        if( hasAlphaTestOrHash && getProperty( kNoTid, PbsProperty::UseTextureAlpha ) )
         {
             if( datablock->mTexturesDescSet )
                 setDetailMapProperties( kNoTid, datablock, inOutPieces, true );
@@ -1288,9 +1289,6 @@ namespace Ogre
             }
         }
 
-        if( getProperty( tid, HlmsBaseProp::BlueNoise ) )
-            setTextureReg( tid, PixelShader, "blueNoise", texUnit++ );
-
         if( getProperty( tid, HlmsBaseProp::EnableDecals ) )
         {
             const int32 decalsDiffuseProp = getProperty( tid, HlmsBaseProp::DecalsDiffuse );
@@ -1345,6 +1343,9 @@ namespace Ogre
             ++texUnit;
         }
 
+        if( getProperty( tid, HlmsBaseProp::BlueNoise ) )
+            setTextureReg( tid, PixelShader, "blueNoise", texUnit++ );
+
         texUnit += mListener->getNumExtraPassTextures( mT[tid].setProperties, casterPass );
 
         setProperty( tid, PbsProperty::Set0TextureSlotEnd, texUnit );
@@ -1392,8 +1393,11 @@ namespace Ogre
             else
                 setProperty( tid, "particleSystemGpuData", mParticleSystemSlot );
 
-            setProperty( kNoTid, HlmsBaseProp::Normal, 1 );
-            setProperty( kNoTid, HlmsBaseProp::Tangent, 1 );
+            if( !casterPass )
+            {
+                setProperty( kNoTid, HlmsBaseProp::Normal, 1 );
+                setProperty( kNoTid, HlmsBaseProp::Tangent, 1 );
+            }
             setProperty( kNoTid, HlmsBaseProp::UvCount, 1 );
         }
     }
@@ -1738,6 +1742,8 @@ namespace Ogre
 #endif
         }
 
+        const bool bNeedsViewMatrix = msHasParticleFX2Plugin;
+
         {
             uint32 numPassConstBuffers = 3u;
             if( !casterPass )
@@ -1749,6 +1755,7 @@ namespace Ogre
 
             if( msHasParticleFX2Plugin )
             {
+                setProperty( kNoTid, HlmsBaseProp::ViewMatrix, 1 );
                 mParticleSystemConstSlot = static_cast<uint8>( numPassConstBuffers );
                 ++numPassConstBuffers;
             }
@@ -1959,6 +1966,9 @@ namespace Ogre
         }
         else
         {
+            // float4x4 view
+            if( bNeedsViewMatrix )
+                mapSize += 16u * 4u;
             isShadowCastingPointLight = getProperty( kNoTid, HlmsBaseProp::ShadowCasterPoint ) != 0;
             // vec4 cameraPosWS
             if( isShadowCastingPointLight )
@@ -2859,6 +2869,12 @@ namespace Ogre
         }
         else
         {
+            if( bNeedsViewMatrix )
+            {
+                for( size_t i = 0u; i < 16u; ++i )
+                    *passBufferPtr++ = (float)viewMatrix[0][i];
+            }
+
             // vec4 viewZRow
             if( mShadowFilter == ExponentialShadowMaps )
             {
@@ -2969,9 +2985,6 @@ namespace Ogre
             if( mLtcMatrixTexture )
                 ++mTexUnitSlotStart;
 
-            if( mHlmsManager->getBlueNoiseTexture() )
-                ++mTexUnitSlotStart;
-
             for( size_t i = 0u; i < 3u; ++i )
             {
                 if( mDecalsTextures[i] && ( i != 2u || !mDecalsDiffuseMergedEmissive ) )
@@ -2989,6 +3002,16 @@ namespace Ogre
                     mTexUnitSlotStart - 1u -
                     mListener->getNumExtraPassTextures( mT[kNoTid].setProperties, casterPass ) );
             }
+#endif
+        }
+
+        if( mHlmsManager->getBlueNoiseTexture() )
+        {
+            // Blue Noise is bound with shadow casters too
+            ++mTexUnitSlotStart;
+#ifdef OGRE_BUILD_COMPONENT_PLANAR_REFLECTIONS
+            // Planar reflections did not account for our slot which comes after it. Correct it.
+            --mPlanarReflectionSlotIdx;
 #endif
         }
 
@@ -3179,13 +3202,6 @@ namespace Ogre
                     ++texUnit;
                 }
 
-                if( mHlmsManager->getBlueNoiseTexture() )
-                {
-                    *commandBuffer->addCommand<CbTexture>() =
-                        CbTexture( (uint16)texUnit, mHlmsManager->getBlueNoiseTexture(), 0 );
-                    ++texUnit;
-                }
-
                 for( size_t i = 0u; i < 3u; ++i )
                 {
                     if( mDecalsTextures[i] && ( i != 2u || !mDecalsDiffuseMergedEmissive ) )
@@ -3218,6 +3234,13 @@ namespace Ogre
                 }
             }
 
+            if( mHlmsManager->getBlueNoiseTexture() )
+            {
+                *commandBuffer->addCommand<CbTexture>() =
+                    CbTexture( (uint16)texUnit, mHlmsManager->getBlueNoiseTexture(), 0 );
+                ++texUnit;
+            }
+
             mLastDescTexture = 0;
             mLastDescSampler = 0;
             mLastBoundPool = 0;
@@ -3246,7 +3269,8 @@ namespace Ogre
         // Don't bind the material buffer on caster passes (important to keep
         // MDI & auto-instancing running on shadow map passes)
         if( mLastBoundPool != datablock->getAssignedPool() &&
-            ( !casterPass || datablock->getAlphaTest() != CMPF_ALWAYS_PASS ) )
+            ( !casterPass || datablock->getAlphaTest() != CMPF_ALWAYS_PASS ||
+              datablock->getAlphaHashing() ) )
         {
             // layout(binding = 1) uniform MaterialBuf {} materialArray
             const ConstBufferPool::BufferPool *newPool = datablock->getAssignedPool();
@@ -3585,7 +3609,8 @@ namespace Ogre
         //                          ---- PIXEL SHADER ----
         //---------------------------------------------------------------------------
 
-        if( !casterPass || datablock->getAlphaTest() != CMPF_ALWAYS_PASS )
+        if( !casterPass || datablock->getAlphaTest() != CMPF_ALWAYS_PASS ||
+            datablock->getAlphaHashing() )
         {
 #ifdef OGRE_BUILD_COMPONENT_PLANAR_REFLECTIONS
             if( !casterPass && mHasPlanarReflections &&

Components/Hlms/Unlit/src/OgreHlmsUnlit.cpp
@@ -538,9 +538,10 @@ namespace Ogre
         //                                              renderable->getDatablock() );
 
         HlmsDatablock *datablock = renderable->getDatablock();
-        const bool hasAlphaTest = datablock->getAlphaTest() != CMPF_ALWAYS_PASS;
+        const bool hasAlphaTestOrHash =
+            datablock->getAlphaTest() != CMPF_ALWAYS_PASS || datablock->getAlphaHashing();
 
-        if( !hasAlphaTest )
+        if( !hasAlphaTestOrHash )
         {
             HlmsPropertyVec::iterator itor = mT[kNoTid].setProperties.begin();
             HlmsPropertyVec::iterator endt = mT[kNoTid].setProperties.end();
@@ -552,9 +553,7 @@ namespace Ogre
                     itor->keyName != HlmsPsoProp::InputLayoutId &&
                     itor->keyName != HlmsBaseProp::Skeleton &&
                     itor->keyName != HlmsBaseProp::BonesPerVertex &&
-                    itor->keyName != HlmsBaseProp::DualParaboloidMapping &&
-                    itor->keyName != HlmsBaseProp::AlphaTest &&
-                    itor->keyName != HlmsBaseProp::AlphaBlend )
+                    itor->keyName != HlmsBaseProp::DualParaboloidMapping )
                 {
                     itor = mT[kNoTid].setProperties.erase( itor );
                     endt = mT[kNoTid].setProperties.end();
@@ -570,6 +569,8 @@ namespace Ogre
             inOutPieces[VertexShader] = normalPassPieces[VertexShader];
             inOutPieces[PixelShader] = normalPassPieces[PixelShader];
         }
+
+        setupSharedBasicProperties( renderable );
 
         if( mFastShaderBuildHack )
             setProperty( kNoTid, UnlitProperty::MaterialsPerBuffer, static_cast<int>( 2 ) );
@@ -1030,7 +1031,9 @@ namespace Ogre
 
         // Don't bind the material buffer on caster passes (important to keep
         // MDI & auto-instancing running on shadow map passes)
-        if( mLastBoundPool != datablock->getAssignedPool() && !casterPass )
+        if( mLastBoundPool != datablock->getAssignedPool() &&
+            ( !casterPass || datablock->getAlphaTest() != CMPF_ALWAYS_PASS ||
+              datablock->getAlphaHashing() ) )
         {
             // layout(binding = 1) uniform MaterialBuf {} materialArray
             const ConstBufferPool::BufferPool *newPool = datablock->getAssignedPool();
@@ -1107,7 +1110,8 @@ namespace Ogre
         //                          ---- PIXEL SHADER ----
         //---------------------------------------------------------------------------
 
-        if( !casterPass || datablock->getAlphaTest() != CMPF_ALWAYS_PASS )
+        if( !casterPass || datablock->getAlphaTest() != CMPF_ALWAYS_PASS ||
+            datablock->getAlphaHashing() )
         {
             if( datablock->mTexturesDescSet != mLastDescTexture )
             {

OgreMain/include/OgreHlms.h
@@ -841,6 +841,10 @@ namespace Ogre
         */
         static bool findParamInVec( const HlmsParamVec &paramVec, IdString key, String &inOut );
 
+    protected:
+        void setupSharedBasicProperties( Renderable *renderable );
+
+    public:
         /** Called by the renderable when either it changes the material,
             or its properties change (e.g. the mesh' uvs are stripped)
         @param renderable

OgreMain/src/OgreHlms.cpp
@@ -2766,26 +2766,8 @@ namespace Ogre
         }
     }
     //-----------------------------------------------------------------------------------
-    void Hlms::calculateHashFor( Renderable *renderable, uint32 &outHash, uint32 &outCasterHash )
+    void Hlms::setupSharedBasicProperties( Renderable *renderable )
     {
-        OgreProfileExhaustive( "Hlms::calculateHashFor" );
-
-        mT[kNoTid].setProperties.clear();
-
-        setProperty( kNoTid, HlmsBaseProp::Skeleton, renderable->hasSkeletonAnimation() );
-
-        setProperty( kNoTid, HlmsBaseProp::Pose, renderable->getNumPoses() );
-        setProperty( kNoTid, HlmsBaseProp::PoseHalfPrecision, renderable->getPoseHalfPrecision() );
-        setProperty( kNoTid, HlmsBaseProp::PoseNormals, renderable->getPoseNormals() );
-
-        uint16 numTexCoords = 0;
-        if( renderable->getVaos( VpNormal ).empty() )
-            numTexCoords = calculateHashForV1( renderable );
-        else
-            numTexCoords = calculateHashForV2( renderable );
-
-        setProperty( kNoTid, HlmsBaseProp::UvCount, numTexCoords );
-
         HlmsDatablock *datablock = renderable->getDatablock();
 
         for( size_t i = 0u; i < NumShaderTypes; ++i )
@@ -2856,11 +2838,34 @@ namespace Ogre
             setProperty( kNoTid, HlmsBaseProp::IdentityViewProjDynamic, 1 );
         else if( renderable->getUseIdentityProjection() )
             setProperty( kNoTid, HlmsBaseProp::IdentityViewProj, 1 );
+    }
+    //-----------------------------------------------------------------------------------
+    void Hlms::calculateHashFor( Renderable *renderable, uint32 &outHash, uint32 &outCasterHash )
+    {
+        OgreProfileExhaustive( "Hlms::calculateHashFor" );
 
-        setProperty( kNoTid, HlmsPsoProp::Macroblock,
-                     renderable->getDatablock()->getMacroblock( false )->mLifetimeId );
-        setProperty( kNoTid, HlmsPsoProp::Blendblock,
-                     renderable->getDatablock()->getBlendblock( false )->mLifetimeId );
+        mT[kNoTid].setProperties.clear();
+
+        setProperty( kNoTid, HlmsBaseProp::Skeleton, renderable->hasSkeletonAnimation() );
+
+        setProperty( kNoTid, HlmsBaseProp::Pose, renderable->getNumPoses() );
+        setProperty( kNoTid, HlmsBaseProp::PoseHalfPrecision, renderable->getPoseHalfPrecision() );
+        setProperty( kNoTid, HlmsBaseProp::PoseNormals, renderable->getPoseNormals() );
+
+        uint16 numTexCoords = 0;
+        if( renderable->getVaos( VpNormal ).empty() )
+            numTexCoords = calculateHashForV1( renderable );
+        else
+            numTexCoords = calculateHashForV2( renderable );
+
+        setProperty( kNoTid, HlmsBaseProp::UvCount, numTexCoords );
+
+        setupSharedBasicProperties( renderable );
+
+        HlmsDatablock *datablock = renderable->getDatablock();
+
+        setProperty( kNoTid, HlmsPsoProp::Macroblock, datablock->getMacroblock( false )->mLifetimeId );
+        setProperty( kNoTid, HlmsPsoProp::Blendblock, datablock->getBlendblock( false )->mLifetimeId );
 
         PiecesMap pieces[NumShaderTypes];
         if( datablock->getAlphaTest() != CMPF_ALWAYS_PASS )
@@ -2892,10 +2897,8 @@ namespace Ogre
             setProperty( kNoTid, HlmsPsoProp::InputLayoutId, vao->getInputLayoutId() );
         }
         calculateHashForPreCaster( renderable, piecesCaster, pieces );
-        setProperty( kNoTid, HlmsPsoProp::Macroblock,
-                     renderable->getDatablock()->getMacroblock( true )->mLifetimeId );
-        setProperty( kNoTid, HlmsPsoProp::Blendblock,
-                     renderable->getDatablock()->getBlendblock( true )->mLifetimeId );
+        setProperty( kNoTid, HlmsPsoProp::Macroblock, datablock->getMacroblock( true )->mLifetimeId );
+        setProperty( kNoTid, HlmsPsoProp::Blendblock, datablock->getBlendblock( true )->mLifetimeId );
         uint32 renderableCasterHash = this->addRenderableCache( mT[kNoTid].setProperties, piecesCaster );
 
         outHash = renderableHash;

OgreMain/include/Math/Array/C/OgreArrayAabb.h
@@ -167,6 +167,7 @@ namespace Ogre
         inline ArrayReal distance( const ArrayVector3 &v ) const;
 
         static const ArrayAabb BOX_INFINITE;
+        static const ArrayAabb BOX_NULL;
 
         // Contains all zeroes. Used for inactive objects to avoid causing unnecessary NaNs
         static const ArrayAabb BOX_ZERO;

OgreMain/include/Math/Array/NEON/Single/OgreArrayAabb.h
@@ -167,6 +167,7 @@ namespace Ogre
         inline ArrayReal distance( const ArrayVector3 &v ) const;
 
         static const ArrayAabb BOX_INFINITE;
+        static const ArrayAabb BOX_NULL;
 
         // Contains all zeroes. Used for inactive objects to avoid causing unnecessary NaNs
         static const ArrayAabb BOX_ZERO;

OgreMain/include/Math/Array/SSE2/Single/OgreArrayAabb.h
@@ -167,6 +167,7 @@ namespace Ogre
         inline ArrayReal distance( const ArrayVector3 &v ) const;
 
         static const ArrayAabb BOX_INFINITE;
+        static const ArrayAabb BOX_NULL;
 
         // Contains all zeroes. Used for inactive objects to avoid causing unnecessary NaNs
         static const ArrayAabb BOX_ZERO;

OgreMain/include/Math/Simple/C/OgreAabb.inl
@@ -69,9 +69,9 @@ namespace Ogre
         Vector3 min( mCenter - mHalfSize );
         min.makeFloor( rhs.mCenter - rhs.mHalfSize );
 
-        if( max.x != std::numeric_limits<float>::infinity() &&
-            max.y != std::numeric_limits<float>::infinity() &&
-            max.z != std::numeric_limits<float>::infinity() )
+        if( std::abs( max.x ) != std::numeric_limits<float>::infinity() &&
+            std::abs( max.y ) != std::numeric_limits<float>::infinity() &&
+            std::abs( max.z ) != std::numeric_limits<float>::infinity() )
         {
             mCenter = ( max + min ) * 0.5f;
         }

commit 9df4a3fe4036236bee2d43f61d30f015258bf227

[Regression] Broken Blending during shadow caster passes
Fix setupSharedBasicProperties setting wrong properties for shadow
casters.

Thanks Eugene for everything!

Bug was introduced in 188068f5b64f6c699d76920d8642522051473bc6

Components/Hlms/Pbs/src/OgreHlmsPbs.cpp
@@ -1086,7 +1086,7 @@ namespace Ogre
             }
         }
 
-        setupSharedBasicProperties( renderable );
+        setupSharedBasicProperties( renderable, true );
 
         if( hasAlphaTestOrHash && getProperty( kNoTid, PbsProperty::UseTextureAlpha ) )
         {

Components/Hlms/Unlit/src/OgreHlmsUnlit.cpp
@@ -570,7 +570,7 @@ namespace Ogre
             inOutPieces[PixelShader] = normalPassPieces[PixelShader];
         }
 
-        setupSharedBasicProperties( renderable );
+        setupSharedBasicProperties( renderable, true );
 
         if( mFastShaderBuildHack )
             setProperty( kNoTid, UnlitProperty::MaterialsPerBuffer, static_cast<int>( 2 ) );

OgreMain/include/OgreHlms.h
@@ -859,7 +859,7 @@ namespace Ogre
         static bool findParamInVec( const HlmsParamVec &paramVec, IdString key, String &inOut );
 
     protected:
-        void setupSharedBasicProperties( Renderable *renderable );
+        void setupSharedBasicProperties( Renderable *renderable, const bool bCasterPass );
 
     public:
         /** Called by the renderable when either it changes the material,

OgreMain/src/OgreHlms.cpp
@@ -2763,7 +2763,7 @@ namespace Ogre
         }
     }
     //-----------------------------------------------------------------------------------
-    void Hlms::setupSharedBasicProperties( Renderable *renderable )
+    void Hlms::setupSharedBasicProperties( Renderable *renderable, const bool bCasterPass )
     {
         HlmsDatablock *datablock = renderable->getDatablock();
 
@@ -2779,9 +2779,9 @@ namespace Ogre
         setProperty( kNoTid, HlmsBaseProp::AlphaTestShadowCasterOnly,
                      datablock->getAlphaTestShadowCasterOnly() );
         setProperty( kNoTid, HlmsBaseProp::AlphaBlend,
-                     datablock->getBlendblock( false )->isAutoTransparent() );
+                     datablock->getBlendblock( bCasterPass )->isAutoTransparent() );
         setProperty( kNoTid, HlmsBaseProp::AlphaToCoverage,
-                     datablock->getBlendblock( false )->mAlphaToCoverage );
+                     datablock->getBlendblock( bCasterPass )->mAlphaToCoverage );
         if( datablock->getAlphaHashing() )
             setProperty( kNoTid, HlmsBaseProp::AlphaHash, 1 );
 
@@ -2857,7 +2857,7 @@ namespace Ogre
 
         setProperty( kNoTid, HlmsBaseProp::UvCount, numTexCoords );
 
-        setupSharedBasicProperties( renderable );
+        setupSharedBasicProperties( renderable, false );
 
         HlmsDatablock *datablock = renderable->getDatablock();
 