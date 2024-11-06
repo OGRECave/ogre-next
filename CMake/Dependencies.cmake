OGRECave/ogre

CMake: use PROJECT_*_DIR and remove superficial project() calls

CMake/Dependencies.cmake
@@ -23,37 +23,37 @@ if(OGRE_BUILD_PLATFORM_EMSCRIPTEN)
     ${OGRE_DEPENDENCIES_DIR}
     ${EMSCRIPTEN_ROOT_PATH}/system
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/EmscriptenDependencies"
-    "${OGRE_SOURCE_DIR}/EmscriptenDependencies"
-    "${OGRE_BINARY_DIR}/../EmscriptenDependencies"
-    "${OGRE_SOURCE_DIR}/../EmscriptenDependencies"
+    "${PROJECT_BINARY_DIR}/EmscriptenDependencies"
+    "${PROJECT_SOURCE_DIR}/EmscriptenDependencies"
+    "${PROJECT_BINARY_DIR}/../EmscriptenDependencies"
+    "${PROJECT_SOURCE_DIR}/../EmscriptenDependencies"
   )
 elseif(APPLE_IOS)
   set(OGRE_DEP_SEARCH_PATH 
     ${OGRE_DEPENDENCIES_DIR}
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/iOSDependencies"
-    "${OGRE_SOURCE_DIR}/iOSDependencies"
-    "${OGRE_BINARY_DIR}/../iOSDependencies"
-    "${OGRE_SOURCE_DIR}/../iOSDependencies"
+    "${PROJECT_BINARY_DIR}/iOSDependencies"
+    "${PROJECT_SOURCE_DIR}/iOSDependencies"
+    "${PROJECT_BINARY_DIR}/../iOSDependencies"
+    "${PROJECT_SOURCE_DIR}/../iOSDependencies"
   )
 elseif(OGRE_BUILD_PLATFORM_ANDROID)
   set(OGRE_DEP_SEARCH_PATH 
     ${OGRE_DEPENDENCIES_DIR}
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/AndroidDependencies"
-    "${OGRE_SOURCE_DIR}/AndroidDependencies"
-    "${OGRE_BINARY_DIR}/../AndroidDependencies"
-    "${OGRE_SOURCE_DIR}/../AndroidDependencies"
+    "${PROJECT_BINARY_DIR}/AndroidDependencies"
+    "${PROJECT_SOURCE_DIR}/AndroidDependencies"
+    "${PROJECT_BINARY_DIR}/../AndroidDependencies"
+    "${PROJECT_SOURCE_DIR}/../AndroidDependencies"
   )
 else()
   set(OGRE_DEP_SEARCH_PATH 
     ${OGRE_DEPENDENCIES_DIR}
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/Dependencies"
-    "${OGRE_SOURCE_DIR}/Dependencies"
-    "${OGRE_BINARY_DIR}/../Dependencies"
-    "${OGRE_SOURCE_DIR}/../Dependencies"
+    "${PROJECT_BINARY_DIR}/Dependencies"
+    "${PROJECT_SOURCE_DIR}/Dependencies"
+    "${PROJECT_BINARY_DIR}/../Dependencies"
+    "${PROJECT_SOURCE_DIR}/../Dependencies"
   )
 endif()
 
@@ -106,10 +106,10 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
         message(STATUS "Building zlib")
         file(DOWNLOAD 
             http://zlib.net/zlib-1.2.11.tar.gz
-            ${OGRE_BINARY_DIR}/zlib-1.2.11.tar.gz 
+            ${PROJECT_BINARY_DIR}/zlib-1.2.11.tar.gz 
             EXPECTED_MD5 1c9f62f0778697a09d36121ead88e08e)
         execute_process(COMMAND ${CMAKE_COMMAND} 
-            -E tar xf zlib-1.2.11.tar.gz WORKING_DIRECTORY ${OGRE_BINARY_DIR})
+            -E tar xf zlib-1.2.11.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
         execute_process(COMMAND ${CMAKE_COMMAND}
             -DCMAKE_INSTALL_PREFIX=${OGREDEPS_PATH}
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -117,18 +117,18 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
             -G ${CMAKE_GENERATOR}
             -DCMAKE_GENERATOR_PLATFORM=${CMAKE_GENERATOR_PLATFORM}
             ${CROSS}
-            ${OGRE_BINARY_DIR}/zlib-1.2.11
-            WORKING_DIRECTORY ${OGRE_BINARY_DIR}/zlib-1.2.11)
+            ${PROJECT_BINARY_DIR}/zlib-1.2.11
+            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/zlib-1.2.11)
         execute_process(COMMAND ${CMAKE_COMMAND} 
-            --build ${OGRE_BINARY_DIR}/zlib-1.2.11 ${BUILD_COMMAND_OPTS})
+            --build ${PROJECT_BINARY_DIR}/zlib-1.2.11 ${BUILD_COMMAND_OPTS})
     endif()
 
     message(STATUS "Building ZZIPlib")
     file(DOWNLOAD
         https://github.com/paroj/ZZIPlib/archive/master.tar.gz
-        ${OGRE_BINARY_DIR}/ZZIPlib-master.tar.gz)
+        ${PROJECT_BINARY_DIR}/ZZIPlib-master.tar.gz)
     execute_process(COMMAND ${CMAKE_COMMAND}
-        -E tar xf ZZIPlib-master.tar.gz WORKING_DIRECTORY ${OGRE_BINARY_DIR})
+        -E tar xf ZZIPlib-master.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
     execute_process(COMMAND ${CMAKE_COMMAND}
         -DCMAKE_INSTALL_PREFIX=${OGREDEPS_PATH}
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -137,22 +137,22 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
         -G ${CMAKE_GENERATOR}
         -DCMAKE_GENERATOR_PLATFORM=${CMAKE_GENERATOR_PLATFORM}
         ${CROSS}
-        ${OGRE_BINARY_DIR}/ZZIPlib-master
-        WORKING_DIRECTORY ${OGRE_BINARY_DIR}/ZZIPlib-master)
+        ${PROJECT_BINARY_DIR}/ZZIPlib-master
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/ZZIPlib-master)
     execute_process(COMMAND ${CMAKE_COMMAND} 
-        --build ${OGRE_BINARY_DIR}/ZZIPlib-master ${BUILD_COMMAND_OPTS})
+        --build ${PROJECT_BINARY_DIR}/ZZIPlib-master ${BUILD_COMMAND_OPTS})
     
     message(STATUS "Building freetype")
     file(DOWNLOAD
         https://download.savannah.gnu.org/releases/freetype/freetype-2.9.tar.gz
-        ${OGRE_BINARY_DIR}/freetype-2.9.tar.gz)
+        ${PROJECT_BINARY_DIR}/freetype-2.9.tar.gz)
     execute_process(COMMAND ${CMAKE_COMMAND}
-        -E tar xf freetype-2.9.tar.gz WORKING_DIRECTORY ${OGRE_BINARY_DIR})
+        -E tar xf freetype-2.9.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
     # patch toolchain for iOS
     execute_process(COMMAND ${CMAKE_COMMAND} -E copy
-        ${OGRE_SOURCE_DIR}/CMake/toolchain/ios.toolchain.xcode.cmake
+        ${PROJECT_SOURCE_DIR}/CMake/toolchain/ios.toolchain.xcode.cmake
         freetype-2.9/builds/cmake/iOS.cmake
-		WORKING_DIRECTORY ${OGRE_BINARY_DIR})
+		WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
     execute_process(COMMAND ${CMAKE_COMMAND}
         -DCMAKE_INSTALL_PREFIX=${OGREDEPS_PATH}
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -160,24 +160,24 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
         -DWITH_PNG=OFF
         -DWITH_BZip2=OFF # tries to use it on iOS otherwise
         # workaround for broken iOS toolchain in freetype
-        -DPROJECT_SOURCE_DIR=${OGRE_BINARY_DIR}/freetype-2.9
+        -DPROJECT_SOURCE_DIR=${PROJECT_BINARY_DIR}/freetype-2.9
         ${CROSS}
         -G ${CMAKE_GENERATOR}
         -DCMAKE_GENERATOR_PLATFORM=${CMAKE_GENERATOR_PLATFORM}
-        ${OGRE_BINARY_DIR}/freetype-2.9
-        WORKING_DIRECTORY ${OGRE_BINARY_DIR}/freetype-2.9/objs)
+        ${PROJECT_BINARY_DIR}/freetype-2.9
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/freetype-2.9/objs)
     execute_process(COMMAND ${CMAKE_COMMAND}
-        --build ${OGRE_BINARY_DIR}/freetype-2.9/objs ${BUILD_COMMAND_OPTS})
+        --build ${PROJECT_BINARY_DIR}/freetype-2.9/objs ${BUILD_COMMAND_OPTS})
 
     if(MSVC OR MINGW) # other platforms dont need this
         message(STATUS "Building SDL2")
         file(DOWNLOAD
             https://libsdl.org/release/SDL2-2.0.8.tar.gz
-            ${OGRE_BINARY_DIR}/SDL2-2.0.8.tar.gz)
+            ${PROJECT_BINARY_DIR}/SDL2-2.0.8.tar.gz)
         execute_process(COMMAND ${CMAKE_COMMAND} 
-            -E tar xf SDL2-2.0.8.tar.gz WORKING_DIRECTORY ${OGRE_BINARY_DIR})
+            -E tar xf SDL2-2.0.8.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
         execute_process(COMMAND ${CMAKE_COMMAND}
-            -E make_directory ${OGRE_BINARY_DIR}/SDL2-build)
+            -E make_directory ${PROJECT_BINARY_DIR}/SDL2-build)
         execute_process(COMMAND ${CMAKE_COMMAND}
             -DCMAKE_INSTALL_PREFIX=${OGREDEPS_PATH}
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -185,10 +185,10 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
             -G ${CMAKE_GENERATOR}
             -DCMAKE_GENERATOR_PLATFORM=${CMAKE_GENERATOR_PLATFORM}
             ${CROSS}
-            ${OGRE_BINARY_DIR}/SDL2-2.0.8
-            WORKING_DIRECTORY ${OGRE_BINARY_DIR}/SDL2-build)
+            ${PROJECT_BINARY_DIR}/SDL2-2.0.8
+            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/SDL2-build)
         execute_process(COMMAND ${CMAKE_COMMAND}
-            --build ${OGRE_BINARY_DIR}/SDL2-build ${BUILD_COMMAND_OPTS})
+            --build ${PROJECT_BINARY_DIR}/SDL2-build ${BUILD_COMMAND_OPTS})
     endif()
 endif()
 
@@ -311,9 +311,9 @@ macro_log_feature(Softimage_FOUND "Softimage" "Softimage SDK needed for building
 # setting everything up, including overriding any of the above
 # findings.
 set(OGREDEPS_RUNTIME_OUTPUT ${OGRE_RUNTIME_OUTPUT})
-if (EXISTS "${OGRE_SOURCE_DIR}/Dependencies/CMakeLists.txt")
+if (EXISTS "${PROJECT_SOURCE_DIR}/Dependencies/CMakeLists.txt")
   add_subdirectory(Dependencies)
-elseif (EXISTS "${OGRE_SOURCE_DIR}/ogredeps/CMakeLists.txt")
+elseif (EXISTS "${PROJECT_SOURCE_DIR}/ogredeps/CMakeLists.txt")
   add_subdirectory(ogredeps)
 endif ()
 

CMake/InstallDependencies.cmake
@@ -50,12 +50,12 @@ endmacro()
 macro(copy_debug INPUT)
   if (EXISTS ${OGRE_DEP_DIR}/lib/debug/${INPUT})
     if (MINGW OR NMAKE)
-      configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${OGRE_BINARY_DIR}/lib/${INPUT} COPYONLY)
+      configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${PROJECT_BINARY_DIR}/lib/${INPUT} COPYONLY)
 	else ()
       if (IS_DIRECTORY ${OGRE_DEP_DIR}/lib/debug/${INPUT})
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/debug/${INPUT} DESTINATION lib/debug)
       else ()
-        configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${OGRE_BINARY_DIR}/lib/debug/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${PROJECT_BINARY_DIR}/lib/debug/${INPUT} COPYONLY)
       endif ()
 	endif ()
   endif ()
@@ -64,16 +64,16 @@ endmacro()
 macro(copy_release INPUT)
   if (EXISTS ${OGRE_DEP_DIR}/lib/release/${INPUT})
     if (MINGW OR NMAKE)
-      configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/${INPUT} COPYONLY)
+      configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/${INPUT} COPYONLY)
 	else ()
       if (IS_DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT})
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT} DESTINATION lib/release CONFIGURATIONS Release None "")
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT} DESTINATION lib/relwithdebinfo CONFIGURATIONS RelWithDebInfo)
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT} DESTINATION lib/minsizerel CONFIGURATIONS MinSizeRel)
       else ()
-        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/release/${INPUT} COPYONLY)
-        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/relwithdebinfo/${INPUT} COPYONLY)
-        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/minsizerel/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/release/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/relwithdebinfo/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/minsizerel/${INPUT} COPYONLY)
       endif ()
 	endif ()
   endif ()
@@ -105,10 +105,10 @@ endif () # OGRE_INSTALL_DEPENDENCIES
   if(WIN32)
     if(OGRE_BUILD_SAMPLES OR OGRE_BUILD_TESTS)
       if(EXISTS "${SDL2_BINARY}")
-	  file(COPY ${SDL2_BINARY} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
-	  file(COPY ${SDL2_BINARY} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-	  file(COPY ${SDL2_BINARY} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-	  file(COPY ${SDL2_BINARY} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+	  file(COPY ${SDL2_BINARY} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
+	  file(COPY ${SDL2_BINARY} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+	  file(COPY ${SDL2_BINARY} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+	  file(COPY ${SDL2_BINARY} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
       endif()
     endif()
 
@@ -220,27 +220,27 @@ if (OGRE_COPY_DEPENDENCIES)
   if (WIN32)
     # copy the required DLLs to the build directory
     file(GLOB DLLS ${OGRE_DEP_DIR}/bin/*.dll)
-    file(COPY ${DLLS} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
-    file(COPY ${DLLS} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-    file(COPY ${DLLS} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-    file(COPY ${DLLS} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+    file(COPY ${DLLS} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
+    file(COPY ${DLLS} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+    file(COPY ${DLLS} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+    file(COPY ${DLLS} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
 
     if (OGRE_BUILD_PLUGIN_CG)
       if (EXISTS ${Cg_BINARY_DBG} AND EXISTS ${Cg_BINARY_REL})
         # if MinGW or NMake, the release/debug cg.dll's would conflict, so just pick one
         if (MINGW OR (CMAKE_GENERATOR STREQUAL "NMake Makefiles"))
           if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-            file(COPY ${Cg_BINARY_DBG} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
+            file(COPY ${Cg_BINARY_DBG} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
           else ()
-            file(COPY ${Cg_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-      			file(COPY ${Cg_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-      			file(COPY ${Cg_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+            file(COPY ${Cg_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+      			file(COPY ${Cg_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+      			file(COPY ${Cg_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
           endif ()
         else ()
-            file(COPY ${Cg_BINARY_DBG} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
-      		file(COPY ${Cg_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-      		file(COPY ${Cg_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-      		file(COPY ${Cg_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+            file(COPY ${Cg_BINARY_DBG} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
+      		file(COPY ${Cg_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+      		file(COPY ${Cg_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+      		file(COPY ${Cg_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
         endif ()
       endif()
     endif()

CMake/Packaging.cmake
@@ -12,7 +12,7 @@ set(CPACK_PACKAGE_VERSION_MAJOR ${OGRE_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${OGRE_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${OGRE_VERSION_PATCH})
 
-set(CPACK_INSTALL_CMAKE_PROJECTS "${OGRE_BINARY_DIR}" "OGRE" "ALL" "/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${PROJECT_BINARY_DIR}" "OGRE" "ALL" "/")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Open Source 3D Graphics Engine")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "OGRE")
 set(CPACK_PACKAGE_NAME "OGRE")
@@ -20,10 +20,10 @@ set(CPACK_PACKAGE_VENDOR "Torus Knot Software")
 
 # CPack won't allow file without recognized extension to be used as
 # license file.
-configure_file("${OGRE_SOURCE_DIR}/LICENSE" "${OGRE_BINARY_DIR}/LICENSE.txt" COPYONLY)
-set(CPACK_RESOURCE_FILE_LICENSE "${OGRE_BINARY_DIR}/LICENSE.txt")
+configure_file("${PROJECT_SOURCE_DIR}/LICENSE" "${PROJECT_BINARY_DIR}/LICENSE.txt" COPYONLY)
+set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_BINARY_DIR}/LICENSE.txt")
 
-#set(CPACK_PACKAGE_ICON "${OGRE_SOURCE_DIR}\\\\ogrelogo.gif")
+#set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}\\\\ogrelogo.gif")
 
 set(CPACK_PACKAGE_CONTACT "OGRE Team <webmaster@ogre3d.org>")
 

OGRECave/ogre-next

This is a port of commit 98563cbaf from ogre to ogre-next, with the
following commands to complete it:
  git grep -l '${OGRE_SOURCE_DIR}' | xargs sed -i 's/${OGRE_SOURCE_DIR}/${PROJECT_SOURCE_DIR}/'
  git grep -l '${OGRE_BINARY_DIR}' | xargs sed -i 's/${OGRE_BINARY_DIR}/${PROJECT_BINARY_DIR}/'
  git grep -l '^PROJECT(' | xargs sed '/^PROJECT(/d' -i
Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

CMake/Dependencies.cmake
@@ -21,38 +21,38 @@ if(OGRE_BUILD_PLATFORM_EMSCRIPTEN)
     ${OGRE_DEPENDENCIES_DIR}
     ${EMSCRIPTEN_ROOT_PATH}/system
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/EmscriptenDependencies"
-    "${OGRE_SOURCE_DIR}/EmscriptenDependencies"
-    "${OGRE_BINARY_DIR}/../EmscriptenDependencies"
-    "${OGRE_SOURCE_DIR}/../EmscriptenDependencies"
+    "${PROJECT_BINARY_DIR}/EmscriptenDependencies"
+    "${PROJECT_SOURCE_DIR}/EmscriptenDependencies"
+    "${PROJECT_BINARY_DIR}/../EmscriptenDependencies"
+    "${PROJECT_SOURCE_DIR}/../EmscriptenDependencies"
   )
   set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} ${OGRE_DEP_SEARCH_PATH})
 elseif(OGRE_BUILD_PLATFORM_APPLE_IOS)
   set(OGRE_DEP_SEARCH_PATH 
     ${OGRE_DEPENDENCIES_DIR}
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/iOSDependencies"
-    "${OGRE_SOURCE_DIR}/iOSDependencies"
-    "${OGRE_BINARY_DIR}/../iOSDependencies"
-    "${OGRE_SOURCE_DIR}/../iOSDependencies"
+    "${PROJECT_BINARY_DIR}/iOSDependencies"
+    "${PROJECT_SOURCE_DIR}/iOSDependencies"
+    "${PROJECT_BINARY_DIR}/../iOSDependencies"
+    "${PROJECT_SOURCE_DIR}/../iOSDependencies"
   )
 elseif(OGRE_BUILD_PLATFORM_ANDROID)
   set(OGRE_DEP_SEARCH_PATH 
     ${OGRE_DEPENDENCIES_DIR}
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/DependenciesAndroid"
-    "${OGRE_SOURCE_DIR}/DependenciesAndroid"
-    "${OGRE_BINARY_DIR}/../DependenciesAndroid"
-    "${OGRE_SOURCE_DIR}/../DependenciesAndroid"
+    "${PROJECT_BINARY_DIR}/DependenciesAndroid"
+    "${PROJECT_SOURCE_DIR}/DependenciesAndroid"
+    "${PROJECT_BINARY_DIR}/../DependenciesAndroid"
+    "${PROJECT_SOURCE_DIR}/../DependenciesAndroid"
   )
 else()
   set(OGRE_DEP_SEARCH_PATH 
     ${OGRE_DEPENDENCIES_DIR}
     ${ENV_OGRE_DEPENDENCIES_DIR}
-    "${OGRE_BINARY_DIR}/Dependencies"
-    "${OGRE_SOURCE_DIR}/Dependencies"
-    "${OGRE_BINARY_DIR}/../Dependencies"
-    "${OGRE_SOURCE_DIR}/../Dependencies"
+    "${PROJECT_BINARY_DIR}/Dependencies"
+    "${PROJECT_SOURCE_DIR}/Dependencies"
+    "${PROJECT_BINARY_DIR}/../Dependencies"
+    "${PROJECT_SOURCE_DIR}/../Dependencies"
   )
 endif()
 
@@ -238,9 +238,9 @@ macro_log_feature(CppUnit_FOUND "CppUnit" "Library for performing unit tests" "h
 # setting everything up, including overriding any of the above
 # findings.
 set(OGREDEPS_RUNTIME_OUTPUT ${OGRE_RUNTIME_OUTPUT})
-if (EXISTS "${OGRE_SOURCE_DIR}/Dependencies/CMakeLists.txt")
+if (EXISTS "${PROJECT_SOURCE_DIR}/Dependencies/CMakeLists.txt")
   add_subdirectory(Dependencies)
-elseif (EXISTS "${OGRE_SOURCE_DIR}/ogredeps/CMakeLists.txt")
+elseif (EXISTS "${PROJECT_SOURCE_DIR}/ogredeps/CMakeLists.txt")
   add_subdirectory(ogredeps)
 endif ()
 

CMake/InstallDependencies.cmake
@@ -52,12 +52,12 @@ endmacro()
 macro(copy_debug INPUT)
   if (EXISTS ${OGRE_DEP_DIR}/lib/debug/${INPUT})
     if (MINGW OR NMAKE)
-      configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${OGRE_BINARY_DIR}/lib/${INPUT} COPYONLY)
+      configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${PROJECT_BINARY_DIR}/lib/${INPUT} COPYONLY)
 	else ()
       if (IS_DIRECTORY ${OGRE_DEP_DIR}/lib/debug/${INPUT})
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/debug/${INPUT} DESTINATION lib/debug)
       else ()
-        configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${OGRE_BINARY_DIR}/lib/debug/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/debug/${INPUT} ${PROJECT_BINARY_DIR}/lib/debug/${INPUT} COPYONLY)
       endif ()
 	endif ()
   endif ()
@@ -66,16 +66,16 @@ endmacro()
 macro(copy_release INPUT)
   if (EXISTS ${OGRE_DEP_DIR}/lib/release/${INPUT})
     if (MINGW OR NMAKE)
-      configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/${INPUT} COPYONLY)
+      configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/${INPUT} COPYONLY)
 	else ()
       if (IS_DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT})
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT} DESTINATION lib/release CONFIGURATIONS Release None "")
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT} DESTINATION lib/relwithdebinfo CONFIGURATIONS RelWithDebInfo)
         install(DIRECTORY ${OGRE_DEP_DIR}/lib/release/${INPUT} DESTINATION lib/minsizerel CONFIGURATIONS MinSizeRel)
       else ()
-        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/release/${INPUT} COPYONLY)
-        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/relwithdebinfo/${INPUT} COPYONLY)
-        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${OGRE_BINARY_DIR}/lib/minsizerel/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/release/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/relwithdebinfo/${INPUT} COPYONLY)
+        configure_file(${OGRE_DEP_DIR}/lib/release/${INPUT} ${PROJECT_BINARY_DIR}/lib/minsizerel/${INPUT} COPYONLY)
       endif ()
 	endif ()
   endif ()
@@ -138,34 +138,34 @@ if (OGRE_COPY_DEPENDENCIES)
     # copy the required DLLs to the build directory (configure_file is the only copy-like op I found in CMake)
 	if( (OGRE_BUILD_SAMPLES2 OR OGRE_BUILD_TESTS) )
 		if(EXISTS ${SDL2_BINARY_DBG} AND EXISTS ${SDL2_BINARY_REL})
-		  file(COPY ${SDL2_BINARY_DBG} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
-		  file(COPY ${SDL2_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-		  file(COPY ${SDL2_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-		  file(COPY ${SDL2_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+		  file(COPY ${SDL2_BINARY_DBG} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
+		  file(COPY ${SDL2_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+		  file(COPY ${SDL2_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+		  file(COPY ${SDL2_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
 		endif()
 		if(EXISTS ${OpenVR_BINARY_DBG} AND EXISTS ${OpenVR_BINARY_REL})
-		  file(COPY ${OpenVR_BINARY_DBG} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
-		  file(COPY ${OpenVR_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-		  file(COPY ${OpenVR_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-		  file(COPY ${OpenVR_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+		  file(COPY ${OpenVR_BINARY_DBG} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
+		  file(COPY ${OpenVR_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+		  file(COPY ${OpenVR_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+		  file(COPY ${OpenVR_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
 		endif()
 	endif()
 
 	if( OGRE_PROFILING_PROVIDER STREQUAL "remotery" )
 		if(EXISTS ${Remotery_BINARY_DBG} AND EXISTS ${Remotery_BINARY_REL})
-		  file(COPY ${Remotery_BINARY_DBG} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
-		  file(COPY ${Remotery_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-		  file(COPY ${Remotery_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-		  file(COPY ${Remotery_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+		  file(COPY ${Remotery_BINARY_DBG} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
+		  file(COPY ${Remotery_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+		  file(COPY ${Remotery_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+		  file(COPY ${Remotery_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
 		endif()
 	endif()
 
 	if( OGRE_CONFIG_AMD_AGS )
 		if(EXISTS ${AMDAGS_BINARY_DBG} AND EXISTS ${AMDAGS_BINARY_REL})
-		  file(COPY ${AMDAGS_BINARY_DBG} DESTINATION ${OGRE_BINARY_DIR}/bin/debug)
-		  file(COPY ${AMDAGS_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/release)
-		  file(COPY ${AMDAGS_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/relwithdebinfo)
-		  file(COPY ${AMDAGS_BINARY_REL} DESTINATION ${OGRE_BINARY_DIR}/bin/minsizerel)
+		  file(COPY ${AMDAGS_BINARY_DBG} DESTINATION ${PROJECT_BINARY_DIR}/bin/debug)
+		  file(COPY ${AMDAGS_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/release)
+		  file(COPY ${AMDAGS_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/relwithdebinfo)
+		  file(COPY ${AMDAGS_BINARY_REL} DESTINATION ${PROJECT_BINARY_DIR}/bin/minsizerel)
 		endif()
 	endif()
    

CMake/Packaging.cmake
@@ -12,7 +12,7 @@ set(CPACK_PACKAGE_VERSION_MAJOR ${OGRE_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${OGRE_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${OGRE_VERSION_PATCH})
 
-set(CPACK_INSTALL_CMAKE_PROJECTS "${OGRE_BINARY_DIR}" "OGRE" "ALL" "/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${PROJECT_BINARY_DIR}" "OGRE" "ALL" "/")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Open Source 3D Graphics Engine")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "OGRE")
 set(CPACK_PACKAGE_NAME "OGRE-Next")
@@ -20,10 +20,10 @@ set(CPACK_PACKAGE_VENDOR "Torus Knot Software")
 
 # CPack won't allow file without recognized extension to be used as
 # license file.
-configure_file("${OGRE_SOURCE_DIR}/COPYING" "${OGRE_BINARY_DIR}/COPYING.txt" COPYONLY)
-set(CPACK_RESOURCE_FILE_LICENSE "${OGRE_BINARY_DIR}/COPYING.txt")
+configure_file("${PROJECT_SOURCE_DIR}/COPYING" "${PROJECT_BINARY_DIR}/COPYING.txt" COPYONLY)
+set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_BINARY_DIR}/COPYING.txt")
 
-#set(CPACK_PACKAGE_ICON "${OGRE_SOURCE_DIR}\\\\ogrelogo.gif")
+#set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}\\\\ogrelogo.gif")
 
 set(CPACK_PACKAGE_CONTACT "OGRE-Next Team <webmaster@ogre3d.org>")
 