bd2638ae28944da07a1b00695ab5dedbfd5c93d4
darksylinc@users.noreply.github.com <darksylinc>
2024-11-11 13:40:06 +0000

    Deploy GH

--- api/latest/class_ogre_1_1_d3_d11_device_resource.js
+++
@@ -1,5 +0,0 @@
-var class_ogre_1_1_d3_d11_device_resource =
-[
-    [ "notifyDeviceLost", "class_ogre_1_1_d3_d11_device_resource.html#af7b80bd50e600a6829d73c326d98cfae", null ],
-    [ "notifyDeviceRestored", "class_ogre_1_1_d3_d11_device_resource.html#a9f225cbab88da815833e3e006d061c2e", null ]
-];
--- api/latest/search/classes_14.js
+++ api/latest/search/classes_14.js
@@ -1,17 +1,17 @@
 var searchData=
 [
-  ['uavbufferpacked_16250',['UavBufferPacked',['../class_ogre_1_1_uav_buffer_packed.html',1,'Ogre']]],
-  ['uavdependency_16251',['UavDependency',['../struct_ogre_1_1_compositor_pass_def_1_1_uav_dependency.html',1,'Ogre::CompositorPassDef']]],
-  ['uint4_16252',['uint4',['../struct_ogre_1_1uint4.html',1,'Ogre']]],
-  ['unifiedhighlevelgpuprogram_16253',['UnifiedHighLevelGpuProgram',['../class_ogre_1_1_unified_high_level_gpu_program.html',1,'Ogre']]],
-  ['unifiedhighlevelgpuprogramfactory_16254',['UnifiedHighLevelGpuProgramFactory',['../class_ogre_1_1_unified_high_level_gpu_program_factory.html',1,'Ogre']]],
-  ['uniformscalabletask_16255',['UniformScalableTask',['../class_ogre_1_1_uniform_scalable_task.html',1,'Ogre']]],
-  ['unimplementedexception_16256',['UnimplementedException',['../class_ogre_1_1_unimplemented_exception.html',1,'Ogre']]],
-  ['unlitproperty_16257',['UnlitProperty',['../struct_ogre_1_1_unlit_property.html',1,'Ogre']]],
-  ['unsafeblock_16258',['UnsafeBlock',['../struct_ogre_1_1_metal_discard_buffer_manager_1_1_unsafe_block.html',1,'Ogre::MetalDiscardBufferManager::UnsafeBlock'],['../struct_ogre_1_1_vulkan_discard_buffer_manager_1_1_unsafe_block.html',1,'Ogre::VulkanDiscardBufferManager::UnsafeBlock']]],
-  ['updatelodrequest_16259',['UpdateLodRequest',['../struct_ogre_1_1_update_lod_request.html',1,'Ogre']]],
-  ['updatetransformrequest_16260',['UpdateTransformRequest',['../struct_ogre_1_1_update_transform_request.html',1,'Ogre']]],
-  ['uploadfromstagingtex_16261',['UploadFromStagingTex',['../class_ogre_1_1_obj_cmd_buffer_1_1_upload_from_staging_tex.html',1,'Ogre::ObjCmdBuffer']]],
-  ['userobjectbindings_16262',['UserObjectBindings',['../class_ogre_1_1_user_object_bindings.html',1,'Ogre']]],
-  ['utf8_16263',['UTF8',['../structrapidjson_1_1_u_t_f8.html',1,'rapidjson']]]
+  ['uavbufferpacked_16253',['UavBufferPacked',['../class_ogre_1_1_uav_buffer_packed.html',1,'Ogre']]],
+  ['uavdependency_16254',['UavDependency',['../struct_ogre_1_1_compositor_pass_def_1_1_uav_dependency.html',1,'Ogre::CompositorPassDef']]],
+  ['uint4_16255',['uint4',['../struct_ogre_1_1uint4.html',1,'Ogre']]],
+  ['unifiedhighlevelgpuprogram_16256',['UnifiedHighLevelGpuProgram',['../class_ogre_1_1_unified_high_level_gpu_program.html',1,'Ogre']]],
+  ['unifiedhighlevelgpuprogramfactory_16257',['UnifiedHighLevelGpuProgramFactory',['../class_ogre_1_1_unified_high_level_gpu_program_factory.html',1,'Ogre']]],
+  ['uniformscalabletask_16258',['UniformScalableTask',['../class_ogre_1_1_uniform_scalable_task.html',1,'Ogre']]],
+  ['unimplementedexception_16259',['UnimplementedException',['../class_ogre_1_1_unimplemented_exception.html',1,'Ogre']]],
+  ['unlitproperty_16260',['UnlitProperty',['../struct_ogre_1_1_unlit_property.html',1,'Ogre']]],
+  ['unsafeblock_16261',['UnsafeBlock',['../struct_ogre_1_1_metal_discard_buffer_manager_1_1_unsafe_block.html',1,'Ogre::MetalDiscardBufferManager::UnsafeBlock'],['../struct_ogre_1_1_vulkan_discard_buffer_manager_1_1_unsafe_block.html',1,'Ogre::VulkanDiscardBufferManager::UnsafeBlock']]],
+  ['updatelodrequest_16262',['UpdateLodRequest',['../struct_ogre_1_1_update_lod_request.html',1,'Ogre']]],
+  ['updatetransformrequest_16263',['UpdateTransformRequest',['../struct_ogre_1_1_update_transform_request.html',1,'Ogre']]],
+  ['uploadfromstagingtex_16264',['UploadFromStagingTex',['../class_ogre_1_1_obj_cmd_buffer_1_1_upload_from_staging_tex.html',1,'Ogre::ObjCmdBuffer']]],
+  ['userobjectbindings_16265',['UserObjectBindings',['../class_ogre_1_1_user_object_bindings.html',1,'Ogre']]],
+  ['utf8_16266',['UTF8',['../structrapidjson_1_1_u_t_f8.html',1,'rapidjson']]]
 ];
