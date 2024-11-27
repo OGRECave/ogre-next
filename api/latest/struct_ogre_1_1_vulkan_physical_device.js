6b0cfea7efae920755126499f782f3988808f0bb
darksylinc@users.noreply.github.com <darksylinc>
2024-11-27 17:22:46 +0000

    Deploy GH

--- api/latest/struct_ogre_1_1_vulkan_physical_device.js
+++ api/latest/struct_ogre_1_1_vulkan_physical_device.js
@@ -1,6 +1,6 @@
 var struct_ogre_1_1_vulkan_physical_device =
 [
     [ "physicalDevice", "struct_ogre_1_1_vulkan_physical_device.html#a96dc259e80dfcf2713d36b5e8047b2d1", null ],
-    [ "physicalDeviceID", "struct_ogre_1_1_vulkan_physical_device.html#ac4180c53c780357b62b8835ea5598d29", null ],
+    [ "physicalDeviceID", "struct_ogre_1_1_vulkan_physical_device.html#a07969318b9baf802ad8d71c70ca9ee53", null ],
     [ "title", "struct_ogre_1_1_vulkan_physical_device.html#a2f722cd10525ee9cb88fc261bcdb5619", null ]
 ];
