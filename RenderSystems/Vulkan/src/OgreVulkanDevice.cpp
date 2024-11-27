87c952394b72e6071831566d1740243e0368cb05
Eugene Golushkov <e.golushkov@nospam-gmail.com>
2024-11-27 22:34:53 +0100

    [Vk] obtain physicalDeviceID from VkPhysicalDeviceVulkan11Properties, requires Vulkan 1.2 physical device api version

--- RenderSystems/Vulkan/src/OgreVulkanDevice.cpp
+++ RenderSystems/Vulkan/src/OgreVulkanDevice.cpp
@@ -424,6 +424,11 @@ namespace Ogre
                          "VulkanRenderSystem::getVkPhysicalDevices" );
         }
 
+        PFN_vkGetPhysicalDeviceProperties2KHR GetPhysicalDeviceProperties2 = 0;
+        if( hasExtension( VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME ) )
+            GetPhysicalDeviceProperties2 = (PFN_vkGetPhysicalDeviceProperties2KHR)vkGetInstanceProcAddr(
+                mVkInstance, "vkGetPhysicalDeviceProperties2KHR" );
+
         // assign unique names, allowing reordering/inserting/removing
         map<String, unsigned>::type sameNameCounter;
         mVulkanPhysicalDevices.clear();
@@ -438,9 +443,26 @@ namespace Ogre
             if( sameNameIndex != 0 )
                 name += " (" + Ogre::StringConverter::toString( sameNameIndex + 1 ) + ")";
 
-            // TODO: use deviceLUID or deviceUUID if available
+            // { deviceLUID, 0 } on Windows, deviceUUID or name hash otherwise
             uint64 uid[2] = {};
-            OGRE_HASH128_FUNC( name.c_str(), (int)name.size(), IdString::Seed, uid );
+            if( GetPhysicalDeviceProperties2 && deviceProps.apiVersion >= VK_API_VERSION_1_2 )
+            {
+                // VkPhysicalDeviceIDProperties requires enabled VK_KHR_EXTERNAL_xxx extensions
+                VkPhysicalDeviceProperties2 deviceProps2;
+                VkPhysicalDeviceVulkan11Properties vulkan11Props;  // requires VK_API_VERSION_1_2
+                makeVkStruct( deviceProps2, VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2 );
+                makeVkStruct( vulkan11Props, VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_1_PROPERTIES );
+                deviceProps2.pNext = &vulkan11Props;
+                GetPhysicalDeviceProperties2( device, &deviceProps2 );
+                if( vulkan11Props.deviceLUIDValid )
+                    memcpy( uid, vulkan11Props.deviceLUID, VK_LUID_SIZE );
+                else
+                    memcpy( uid, vulkan11Props.deviceUUID, VK_UUID_SIZE );
+            }
+            else
+            {
+                OGRE_HASH128_FUNC( name.c_str(), (int)name.size(), IdString::Seed, uid );
+            }
 
             LogManager::getSingleton().logMessage( "Vulkan: \"" + name + "\"" );
             mVulkanPhysicalDevices.push_back( { device, { uid[0], uid[1] }, name } );
