OGRECave/ogre-next

commit cbfd9e5d08d61c9427ff91372af4551e2e5548ab

[Vk] Add microcode cache

RenderSystems/Vulkan/src/OgreVulkanProgram.cpp
@@ -175,6 +175,11 @@ namespace Ogre
         return EShLangFragment;
     }
     //-----------------------------------------------------------------------
+    String VulkanProgram::getNameForMicrocodeCache( const String &preamble ) const
+    {
+        return mSource + "_" + preamble;
+    }
+    //-----------------------------------------------------------------------
     bool VulkanProgram::extractRootLayoutFromSource()
     {
         if( mRootLayout )
@@ -318,7 +323,81 @@ namespace Ogre
         resources.limits.generalConstantMatrixVectorIndexing = 1;
     }
     //-----------------------------------------------------------------------
-    void VulkanProgram::loadFromSource() { compile( true ); }
+    void VulkanProgram::loadFromSource()
+    {
+        OGRE_ASSERT_LOW( !mCompiled );
+        OGRE_ASSERT_LOW( mSpirv.empty() );
+
+        if( mReplaceVersionMacro )
+            replaceVersionMacros();
+
+        if( mCustomRootLayout && !mReflectArrayRootLayouts )
+        {
+            GpuProgramManager &gpuProgramManager = GpuProgramManager::getSingleton();
+            String preamble;
+            getPreamble( preamble );
+
+            GpuProgramManager::Microcode const *microcodePtr;
+            if( gpuProgramManager.getMicrocodeFromCache( getNameForMicrocodeCache( preamble ),
+                                                         &microcodePtr ) )
+            {
+                const GpuProgramManager::Microcode &microcode = *microcodePtr;
+
+                // We can't use microcode->read() because it's not thread safe.
+                const size_t spirvSizeBytes = microcode->size();
+                mSpirv.resize( spirvSizeBytes / sizeof( uint32 ) );
+                memcpy( mSpirv.data(), microcode->getPtr(), spirvSizeBytes );
+                mCompiled = true;
+
+                LogManager::getSingleton().logMessage( "Shader " + mName + " was in microcode cache." );
+
+                {
+                    VkShaderModuleCreateInfo moduleCi;
+                    makeVkStruct( moduleCi, VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO );
+                    moduleCi.codeSize = mSpirv.size() * sizeof( uint32 );
+                    moduleCi.pCode = mSpirv.data();
+                    VkResult result =
+                        vkCreateShaderModule( mDevice->mDevice, &moduleCi, 0, &mShaderModule );
+                    checkVkResult( result, "vkCreateShaderModule" );
+
+                    setObjectName( mDevice->mDevice, (uint64_t)mShaderModule,
+                                   VK_DEBUG_REPORT_OBJECT_TYPE_IMAGE_EXT, mName.c_str() );
+                }
+
+                if( !mSpirv.empty() && mType == GPT_VERTEX_PROGRAM )
+                {
+                    OgreProfileExhaustive( "VulkanProgram::compile::SpvReflectShaderModule" );
+                    SpvReflectShaderModule module;
+                    memset( &module, 0, sizeof( module ) );
+                    SpvReflectResult result = spvReflectCreateShaderModule(
+                        mSpirv.size() * sizeof( uint32 ), mSpirv.data(), &module );
+                    if( result != SPV_REFLECT_RESULT_SUCCESS )
+                    {
+                        OGRE_EXCEPT( Exception::ERR_RENDERINGAPI_ERROR,
+                                     "spvReflectCreateShaderModule failed on shader " + mName +
+                                         " error code: " + getSpirvReflectError( result ),
+                                     "VulkanProgram::compile" );
+                    }
+
+                    FreeModuleOnDestructor modulePtr( &module );
+                    gatherVertexInputs( module );
+                }
+
+#if OGRE_DEBUG_MODE >= OGRE_DEBUG_MEDIUM
+                gatherArrayedDescs( true );
+#endif
+            }
+            else
+            {
+                // Note: In multithreaded compilation, if two threads try to compile the same
+                // shader, both threads could end up here. When both threads try to add the
+                // microcode, one will be discarded. Last thread wins.
+                compile( true );
+            }
+        }
+        else
+            compile( true );
+    }
     //-----------------------------------------------------------------------
     /**
     @brief VulkanProgram::replaceVersionMacros
@@ -564,12 +643,10 @@ namespace Ogre
         const char *sourceCString = mSource.c_str();
         shader.setStrings( &sourceCString, 1 );
 
+        String preamble;
+
         if( !mCompileError )
         {
-            if( mReplaceVersionMacro )
-                replaceVersionMacros();
-
-            String preamble;
             getPreamble( preamble );
             shader.setPreamble( preamble.c_str() );
 
@@ -677,12 +754,26 @@ namespace Ogre
                          "VulkanProgram::compile" );
         }
 
-        if( mCompiled && !mSpirv.empty() )
+        // TODO: Support SPIR-Vs with mReflectArrayRootLayouts == true or with mCustomRootLayout == false
+        // Is it even worth it? Most shaders won't need and it just adds complexity.
+        if( mCompiled && !mSpirv.empty() && mCustomRootLayout && !mReflectArrayRootLayouts )
         {
+            GpuProgramManager &gpuProgramManager = GpuProgramManager::getSingleton();
+            if( gpuProgramManager.getSaveMicrocodesToCache() )
+            {
+                const uint32 spirvSizeBytes = static_cast<uint32>( mSpirv.size() * sizeof( uint32 ) );
+                GpuProgramManager::Microcode newMicrocode =
+                    gpuProgramManager.createMicrocode( spirvSizeBytes );
+
+                newMicrocode->write( mSpirv.data(), spirvSizeBytes );
+                gpuProgramManager.addMicrocodeToCache( getNameForMicrocodeCache( preamble ),
+                                                       newMicrocode );
+            }
+
             VkShaderModuleCreateInfo moduleCi;
             makeVkStruct( moduleCi, VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO );
             moduleCi.codeSize = mSpirv.size() * sizeof( uint32 );
-            moduleCi.pCode = &mSpirv[0];
+            moduleCi.pCode = mSpirv.data();
             VkResult result = vkCreateShaderModule( mDevice->mDevice, &moduleCi, 0, &mShaderModule );
             checkVkResult( result, "vkCreateShaderModule" );
 
@@ -696,7 +787,7 @@ namespace Ogre
             SpvReflectShaderModule module;
             memset( &module, 0, sizeof( module ) );
             SpvReflectResult result =
-                spvReflectCreateShaderModule( mSpirv.size() * sizeof( uint32 ), &mSpirv[0], &module );
+                spvReflectCreateShaderModule( mSpirv.size() * sizeof( uint32 ), mSpirv.data(), &module );
             if( result != SPV_REFLECT_RESULT_SUCCESS )
             {
                 OGRE_EXCEPT( Exception::ERR_RENDERINGAPI_ERROR,
@@ -721,7 +812,7 @@ namespace Ogre
     {
         mAssemblerProgram = GpuProgramPtr( this, []( GpuProgram * ) {} );
         if( !mCompiled )
-            compile( true );
+            loadFromSource();
     }
     //---------------------------------------------------------------------------
     void VulkanProgram::unloadImpl()
@@ -807,7 +898,7 @@ namespace Ogre
         SpvReflectShaderModule module;
         memset( &module, 0, sizeof( module ) );
         SpvReflectResult result =
-            spvReflectCreateShaderModule( mSpirv.size() * sizeof( uint32 ), &mSpirv[0], &module );
+            spvReflectCreateShaderModule( mSpirv.size() * sizeof( uint32 ), mSpirv.data(), &module );
         if( result != SPV_REFLECT_RESULT_SUCCESS )
         {
             OGRE_EXCEPT( Exception::ERR_RENDERINGAPI_ERROR,
@@ -1138,7 +1229,7 @@ namespace Ogre
         SpvReflectShaderModule module;
         memset( &module, 0, sizeof( module ) );
         SpvReflectResult result =
-            spvReflectCreateShaderModule( mSpirv.size() * sizeof( uint32 ), &mSpirv[0], &module );
+            spvReflectCreateShaderModule( mSpirv.size() * sizeof( uint32 ), mSpirv.data(), &module );
         if( result != SPV_REFLECT_RESULT_SUCCESS )
         {
             OGRE_EXCEPT( Exception::ERR_RENDERINGAPI_ERROR,

OgreMain/include/OgreGpuProgramManager.h
@@ -238,6 +238,16 @@ namespace Ogre
         */
         virtual const Microcode &getMicrocodeFromCache( const String &source ) const;
 
+        /** Retrieves a microcode for a program from the microcode cache.
+        @param source
+            The key of the shader to obtain
+        @param outMicrocode
+            The microcode being queried. Not nullptr if we return true, nullptr otherwise.
+        @return
+            Returns true if the microcode exists in cache. False otherwise
+        */
+        bool getMicrocodeFromCache( const String &source, const Microcode **outMicrocode ) const;
+
         /** Creates a microcode to be later added to the cache.
         @param size The size of the microcode in bytes
         */

OgreMain/src/OgreGpuProgramManager.cpp
@@ -267,6 +267,23 @@ namespace Ogre
         return mMicrocodeCache.find( computeHashWithRenderSystemName( source ) )->second;
     }
     //---------------------------------------------------------------------
+    bool GpuProgramManager::getMicrocodeFromCache( const String &source,
+                                                   const Microcode **outMicrocode ) const
+    {
+        MicrocodeMap ::const_iterator itor =
+            mMicrocodeCache.find( computeHashWithRenderSystemName( source ) );
+        if( itor != mMicrocodeCache.end() )
+        {
+            *outMicrocode = &itor->second;
+            return true;
+        }
+        else
+        {
+            *outMicrocode = nullptr;
+            return false;
+        }
+    }
+    //---------------------------------------------------------------------
     GpuProgramManager::Microcode GpuProgramManager::createMicrocode( const uint32 size ) const
     {
         return Microcode( OGRE_NEW MemoryDataStream( size ) );

RenderSystems/Vulkan/include/OgreVulkanProgram.h
@@ -139,6 +139,8 @@ namespace Ogre
 
         uint32 getEshLanguage() const;
 
+        String getNameForMicrocodeCache( const String &preamble ) const;
+
         /// Returns true if successfully extracted Root Layout from source
         bool extractRootLayoutFromSource();
 

RenderSystems/Vulkan/src/OgreVulkanRenderSystem.cpp
@@ -688,6 +688,8 @@ namespace Ogre
         rsc->setCapability( RSC_ALPHA_TO_COVERAGE );
         rsc->setCapability( RSC_HW_GAMMA );
         rsc->setCapability( RSC_VERTEX_BUFFER_INSTANCE_DATA );
+        // We don't support PSO nor VkShaderModule caches yet, but we do have SPIR-V caches.
+        rsc->setCapability( RSC_CAN_GET_COMPILED_SHADER_BUFFER );
         rsc->setCapability( RSC_EXPLICIT_API );
         rsc->setMaxPointSize( 256 );
 

commit d5e83985ad6479ccf93fccfa9bd7b6c0c2f54a90

[Regression, Vk] Fix some shaders never compiling
If the shader specifies its RootLayout inside its source code, the
shader would never actually be fully compiled.

Fixes TutorialCompute02_UavBuffer.

Regression introduced in cbfd9e5d08d61c9427ff91372af4551e2e5548ab

RenderSystems/Vulkan/src/OgreVulkanProgram.cpp
@@ -779,12 +779,14 @@ namespace Ogre
                          "VulkanProgram::compile" );
         }
 
-        // TODO: Support SPIR-Vs with mReflectArrayRootLayouts == true or with mCustomRootLayout == false
-        // Is it even worth it? Most shaders won't need and it just adds complexity.
-        if( mCompiled && !mSpirv.empty() && mCustomRootLayout && !mReflectArrayRootLayouts )
+        if( mCompiled && !mSpirv.empty() )
         {
             GpuProgramManager &gpuProgramManager = GpuProgramManager::getSingleton();
-            if( gpuProgramManager.getSaveMicrocodesToCache() )
+            // TODO: Support caching SPIR-Vs with mReflectArrayRootLayouts == true or with
+            // mCustomRootLayout == false Is it even worth it? Most shaders won't need and it
+            // just adds complexity.
+            if( gpuProgramManager.getSaveMicrocodesToCache() && mCustomRootLayout &&
+                !mReflectArrayRootLayouts )
             {
                 const uint32 spirvSizeBytes = static_cast<uint32>( mSpirv.size() * sizeof( uint32 ) );
                 GpuProgramManager::Microcode newMicrocode =