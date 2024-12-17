OGRECave/ogre-next

commit 7255629b6495aa3add9f4ae3cfc1f46909e5d02e

[Vk] Avoid swapchain recreation in windowMovedOrResized() when window is moved but not resized. Swapchains suboptimal due to the mismatching colorspace or orientation are still rebuilt.

RenderSystems/Vulkan/src/Windowing/win32/OgreVulkanWin32Window.cpp
@@ -727,6 +727,9 @@ namespace Ogre
 
         updateWindowRect();
 
+        if( mRequestedWidth == getWidth() && mRequestedHeight == getHeight() && !mRebuildingSwapchain )
+            return;
+
         mDevice->stall();
 
         destroySwapchain();

RenderSystems/Vulkan/src/Windowing/Android/OgreVulkanAndroidWindow.cpp
@@ -241,6 +241,11 @@ namespace Ogre
         if( mClosed || !mNativeWindow )
             return;
 
+        const uint32 newWidth = static_cast<uint32>( ANativeWindow_getWidth( mNativeWindow ) );
+        const uint32 newHeight = static_cast<uint32>( ANativeWindow_getHeight( mNativeWindow ) );
+        if( newWidth == getWidth() && newHeight == getHeight() && !mRebuildingSwapchain )
+            return;
+
         mDevice->stall();
 
 #ifdef OGRE_VULKAN_USE_SWAPPY
@@ -284,9 +289,6 @@ namespace Ogre
         {
             mStencilBuffer->_transitionTo( GpuResidency::OnStorage, (uint8 *)0 );
         }
-
-        const uint32 newWidth = static_cast<uint32>( ANativeWindow_getWidth( mNativeWindow ) );
-        const uint32 newHeight = static_cast<uint32>( ANativeWindow_getHeight( mNativeWindow ) );
 
         setFinalResolution( newWidth, newHeight );
 

RenderSystems/Vulkan/src/Windowing/X11/OgreVulkanXcbWindow.cpp
@@ -517,6 +517,14 @@ namespace Ogre
             mTop = geom->y;
         }
 
+        auto newWidth = geom->width;
+        auto newHeight = geom->height;
+
+        free( geom );
+
+        if( newWidth == getWidth() && newHeight == getHeight() && !mRebuildingSwapchain )
+            return;
+
         mDevice->stall();
 
         destroySwapchain();
@@ -527,11 +535,9 @@ namespace Ogre
         if( mStencilBuffer && mStencilBuffer != mDepthBuffer )
             mStencilBuffer->_transitionTo( GpuResidency::OnStorage, (uint8 *)0 );
 
-        setFinalResolution( geom->width, geom->height );
+        setFinalResolution( newWidth, newHeight );
 
         createSwapchain();
-
-        free( geom );
     }
     //-------------------------------------------------------------------------
     void VulkanXcbWindow::_setVisible( bool visible ) { mVisible = visible; }

commit bf9e2ea808b4f73abf444616d74523e2276d8904

[Vk][REGRESSION] Fix crash on resize
VulkanWin32Window::destroy was checking mRequestedWidth == getWidth() &&
mRequestedHeight == getHeight() but this is always true because
updateWindowRect() would call setFinalResolution.

Bug was introduced in 7255629b6495aa3add9f4ae3cfc1f46909e5d02e

RenderSystems/Vulkan/include/Windowing/win32/OgreVulkanWin32Window.h
@@ -51,7 +51,7 @@ namespace Ogre
 
         static bool mClassRegistered;
 
-        void updateWindowRect();
+        bool updateWindowRect();
         void adjustWindow( uint32 clientWidth, uint32 clientHeight,  //
                            uint32 *outDrawableWidth, uint32 *outDrawableHeight );
 

RenderSystems/Vulkan/src/Windowing/win32/OgreVulkanWin32Window.cpp
@@ -98,8 +98,9 @@ namespace Ogre
         return VK_KHR_WIN32_SURFACE_EXTENSION_NAME;
     }
     //-------------------------------------------------------------------------
-    void VulkanWin32Window::updateWindowRect()
+    bool VulkanWin32Window::updateWindowRect()
     {
+        bool bResolutionChanged = false;
         RECT rc;
         BOOL result;
         result = GetWindowRect( mHwnd, &rc );
@@ -108,7 +109,7 @@ namespace Ogre
             mTop = 0;
             mLeft = 0;
             setFinalResolution( 0, 0 );
-            return;
+            return bResolutionChanged;
         }
 
         mTop = rc.top;
@@ -119,16 +120,19 @@ namespace Ogre
             mTop = 0;
             mLeft = 0;
             setFinalResolution( 0, 0 );
-            return;
+            return bResolutionChanged;
         }
         uint32 width = static_cast<uint32>( rc.right - rc.left );
         uint32 height = static_cast<uint32>( rc.bottom - rc.top );
         if( width != getWidth() || height != getHeight() )
         {
+            bResolutionChanged = true;
             mRequestedWidth = static_cast<uint32>( rc.right - rc.left );
             mRequestedHeight = static_cast<uint32>( rc.bottom - rc.top );
             setFinalResolution( mRequestedWidth, mRequestedHeight );
         }
+
+        return bResolutionChanged;
     }
     //-------------------------------------------------------------------------
     void VulkanWin32Window::destroy()
@@ -712,9 +716,9 @@ namespace Ogre
         if( !mHwnd || IsIconic( mHwnd ) )
             return;
 
-        updateWindowRect();
+        const bool bResolutionChanged = updateWindowRect();
 
-        if( mRequestedWidth == getWidth() && mRequestedHeight == getHeight() && !mRebuildingSwapchain )
+        if( !bResolutionChanged && !mRebuildingSwapchain )
             return;
 
         mDevice->stall();