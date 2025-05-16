3612e7b28de10d9d9cdb8493d5d366d1a6492b84
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2025-05-15 22:23:28 -0300

    Light panel now supports undo

--- Tools/MaterialEditor/src/LightPanel.cpp
+++ Tools/MaterialEditor/src/LightPanel.cpp
@@ -129,8 +129,12 @@ LightPanel::LightPanel( MainWindow *parent ) :
     m_masterNode( 0 ),
     m_secondaryNode( 0 ),
     m_lightNodes{},
+    m_eulerAngles{},
+    m_currPresetIdx( 0u ),
     m_editing( false )
 {
+    EditingScope scope( m_editing );
+
     wxArrayString presets;
     presets.reserve( kNumPresets );
     for( size_t i = 0u; i < kNumPresets; ++i )
@@ -174,19 +178,19 @@ LightPanel::LightPanel( MainWindow *parent ) :
     m_presetChoice->SetSelection( 0 );
     setPreset( 0u );
 
+    m_cameraRelativeCheckbox->SetValue( true );
     setCameraRelative( true );
 }
 //-----------------------------------------------------------------------------
 void LightPanel::reorientLights()
 {
-    double ex = 0.0, ey = 0.0, ez = 0.0;
-    m_eulerX->GetValue().ToDouble( &ex );
-    m_eulerY->GetValue().ToDouble( &ey );
-    m_eulerZ->GetValue().ToDouble( &ez );
+    m_eulerX->GetValue().ToDouble( &m_eulerAngles[0] );
+    m_eulerY->GetValue().ToDouble( &m_eulerAngles[1] );
+    m_eulerZ->GetValue().ToDouble( &m_eulerAngles[2] );
 
-    const Ogre::Quaternion qx( Ogre::Degree( Ogre::Real( ex ) ), Ogre::Vector3::UNIT_X );
-    const Ogre::Quaternion qy( Ogre::Degree( Ogre::Real( ey ) ), Ogre::Vector3::UNIT_Y );
-    const Ogre::Quaternion qz( Ogre::Degree( Ogre::Real( ez ) ), Ogre::Vector3::UNIT_Z );
+    const Ogre::Quaternion qx( Ogre::Degree( Ogre::Real( m_eulerAngles[0] ) ), Ogre::Vector3::UNIT_X );
+    const Ogre::Quaternion qy( Ogre::Degree( Ogre::Real( m_eulerAngles[1] ) ), Ogre::Vector3::UNIT_Y );
+    const Ogre::Quaternion qz( Ogre::Degree( Ogre::Real( m_eulerAngles[2] ) ), Ogre::Vector3::UNIT_Z );
 
     Ogre::Quaternion q = qx * qy * qz;
     q.normalise();
@@ -214,12 +218,18 @@ void LightPanel::OnPresetChoice( wxCommandEvent &event )
         return;
     EditingScope scope( m_editing );
 
+    m_mainWindow->getUndoSystem().pushUndoLights();
     setPreset( uint32_t( m_presetChoice->GetSelection() ) );
     event.Skip();
 }
 //-----------------------------------------------------------------------------
 void LightPanel::OnCheckbox( wxCommandEvent &event )
 {
+    if( m_editing )
+        return;
+    EditingScope scope( m_editing );
+
+    m_mainWindow->getUndoSystem().pushUndoLights();
     setCameraRelative( m_cameraRelativeCheckbox->IsChecked() );
     event.Skip();
 }
@@ -231,6 +241,8 @@ void LightPanel::OnEulerText( wxCommandEvent &event )
     if( m_editing )
         return;
     EditingScope scope( m_editing );
+
+    m_mainWindow->getUndoSystem().pushUndoLights();
 
     for( size_t i = 0u; i < 3u; ++i )
     {
@@ -257,6 +269,17 @@ void LightPanel::OnSlider( wxCommandEvent &event )
         SliderTextWidgetAngle widgets = getEulerSliders( i );
         if( widgets.slider == event.GetEventObject() )
         {
+            if( !m_ignoreUndo )
+            {
+                m_mainWindow->getUndoSystem().pushUndoLights();
+                m_ignoreUndo = true;
+            }
+            if( m_undoMouseUp )
+            {
+                m_ignoreUndo = false;
+                m_undoMouseUp = false;
+            }
+
             widgets.fromSlider();
             reorientLights();
             return;
@@ -266,8 +289,38 @@ void LightPanel::OnSlider( wxCommandEvent &event )
 //-----------------------------------------------------------------------------
 void LightPanel::OnText( wxCommandEvent &event )
 {
-    // TODO: Implement OnText
     event.Skip();
+
+    m_mainWindow->getUndoSystem().pushUndoLights();
+
+    for( size_t i = 0u; i < 3u; ++i )
+    {
+        SliderTextWidgetAngle widgets = getEulerSliders( i );
+        if( widgets.text == event.GetEventObject() )
+        {
+            widgets.fromText();
+            reorientLights();
+            return;
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void LightPanel::UndoMouseUp( wxMouseEvent &event )
+{
+    event.Skip();
+    m_undoMouseUp = true;
+}
+//-----------------------------------------------------------------------------
+void LightPanel::UndoKeyUp( wxKeyEvent &event )
+{
+    event.Skip();
+    m_ignoreUndo = false;
+}
+//-----------------------------------------------------------------------------
+void LightPanel::UndoKillFocus( wxFocusEvent &event )
+{
+    event.Skip();
+    m_ignoreUndo = false;
 }
 //-----------------------------------------------------------------------------
 void LightPanel::setCoordinateConvention( CoordinateConvention::CoordinateConvention newConvention )
@@ -277,25 +330,20 @@ void LightPanel::setCoordinateConvention( CoordinateConvention::CoordinateConven
 //-----------------------------------------------------------------------------
 void LightPanel::setCameraRelative( const bool bCameraRelative )
 {
-    if( m_editing )
-        return;
-    EditingScope scope( m_editing );
-
     m_secondaryNode->getParent()->removeChild( m_secondaryNode );
 
     Ogre::SceneNode *parentNode = 0;
     if( bCameraRelative )
-    {
         parentNode = m_mainWindow->getCameraNode();
-    }
     else
-    {
         parentNode = m_masterNode;
-    }
-
-    m_cameraRelativeCheckbox->SetValue( bCameraRelative );
 
     parentNode->addChild( m_secondaryNode );
+}
+//-----------------------------------------------------------------------------
+bool LightPanel::isCameraRelative() const
+{
+    return m_secondaryNode->getParentSceneNode() != m_masterNode;
 }
 //-----------------------------------------------------------------------------
 void LightPanel::notifyMeshChanged()
@@ -317,6 +365,8 @@ void LightPanel::notifyMeshChanged()
 void LightPanel::setPreset( const uint32_t presetIdx )
 {
     OGRE_ASSERT( presetIdx < kNumPresets );
+
+    m_currPresetIdx = presetIdx;
 
     const Preset &preset = c_presets[presetIdx];
 
@@ -341,13 +391,15 @@ void LightPanel::setPreset( const uint32_t presetIdx )
 //-----------------------------------------------------------------------------
 void LightPanel::loadProject( const rapidjson::Document &d )
 {
+    EditingScope scope( m_editing );
+
     rapidjson::Value::ConstMemberIterator itor = d.FindMember( "lights" );
     if( itor == d.MemberEnd() || !itor->value.IsObject() )
         return;
 
     const rapidjson::Value &lightObj = itor->value;
 
-    lightObj.FindMember( "preset" );
+    itor = lightObj.FindMember( "preset" );
     if( itor != lightObj.MemberEnd() && itor->value.IsUint() &&
         itor->value.GetUint() < m_presetChoice->GetCount() )
     {
@@ -355,11 +407,15 @@ void LightPanel::loadProject( const rapidjson::Document &d )
         setPreset( itor->value.GetUint() );
     }
 
-    lightObj.FindMember( "camera_relative" );
+    itor = lightObj.FindMember( "camera_relative" );
     if( itor != lightObj.MemberEnd() && itor->value.IsBool() )
+    {
+        fprintf( stderr, "cam_rel = %s\n", itor->value.GetBool() ? "true" : "false" );
+        m_cameraRelativeCheckbox->SetValue( itor->value.GetBool() );
         setCameraRelative( itor->value.GetBool() );
+    }
 
-    lightObj.FindMember( "euler" );
+    itor = lightObj.FindMember( "euler" );
     if( itor != lightObj.MemberEnd() && itor->value.IsArray() && itor->value.Size() == 3u &&
         itor->value[0].IsDouble() && itor->value[1].IsDouble() && itor->value[2].IsDouble() )
     {
@@ -378,18 +434,13 @@ void LightPanel::loadProject( const rapidjson::Document &d )
 //-----------------------------------------------------------------------------
 void LightPanel::saveProject( Ogre::String &jsonString )
 {
-    double ex = 0.0, ey = 0.0, ez = 0.0;
-    m_eulerX->GetValue().ToDouble( &ex );
-    m_eulerY->GetValue().ToDouble( &ey );
-    m_eulerZ->GetValue().ToDouble( &ez );
-
     jsonString += ",\n	\"lights\" :\n	{";
     jsonString += "\n		\"preset\" : ";
-    jsonString += std::to_string( m_presetChoice->GetSelection() );
+    jsonString += std::to_string( m_currPresetIdx );
     jsonString += ",\n		\"camera_relative\" : ";
-    jsonString += m_cameraRelativeCheckbox->IsChecked() ? "true" : "false";
+    jsonString += isCameraRelative() ? "true" : "false";
     jsonString += ",\n		\"euler\" : [ ";
-    jsonString +=
-        std::to_string( ex ) + ", " + std::to_string( ey ) + ", " + std::to_string( ez ) + " ]";
+    jsonString += std::to_string( m_eulerAngles[0] ) + ", " + std::to_string( m_eulerAngles[1] ) + ", " +
+                  std::to_string( m_eulerAngles[2] ) + " ]";
     jsonString += "\n	}";
 }
