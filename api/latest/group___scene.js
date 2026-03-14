697c16df0c7425675efe3acc664ffe60fb6fd6d2
darksylinc@users.noreply.github.com <darksylinc>
2026-03-14 15:13:59 +0000

    Deploy GH

--- api/latest/group___scene.js
+++ api/latest/group___scene.js
@@ -194,8 +194,8 @@ var group___scene =
     ] ],
     [ "Ogre::CompositorPassProvider", "class_ogre_1_1_compositor_pass_provider.html", [
       [ "~CompositorPassProvider", "class_ogre_1_1_compositor_pass_provider.html#a220d2b75a54f39299aeb359f56810e58", null ],
-      [ "addPass", "class_ogre_1_1_compositor_pass_provider.html#adb4a50957a720e8292cd0eaba116273b", null ],
-      [ "addPassDef", "class_ogre_1_1_compositor_pass_provider.html#abcb0e0006a30c29f4df718365b97d6b9", null ],
+      [ "addPass", "class_ogre_1_1_compositor_pass_provider.html#a2eacc0f7acbc53a8615cea6f88d312f7", null ],
+      [ "addPassDef", "class_ogre_1_1_compositor_pass_provider.html#a4be2b4e907e1a65c0c864bc9585cbb8d", null ],
       [ "translateCustomPass", "class_ogre_1_1_compositor_pass_provider.html#a3faf3c61e46b3ca1d8dd9e40396c82df", null ],
       [ "translateCustomPass", "class_ogre_1_1_compositor_pass_provider.html#ad4cb882930b2338c0637f5bb4747777c", null ]
     ] ],
